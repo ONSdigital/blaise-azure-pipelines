603e23c1638e821ea5369186404c6fecc8c65de9
Nik Webb <nik.webb@ons.gov.uk>
2021-01-05 09:01:18 +0000

    Update hello-world.ps1

--- hello-world.ps1
+++ hello-world.ps1
@@ -33,4 +33,5 @@ Write-Host "Setting up script and system variables..."
 $metadataVariables = GetMetadataVariables1
 CreateVariables1($metadataVariables)
 Write-Host "##vso[task.setvariable variable=fruit]Apple"
-Write-Host "Fruit is $(env:DRIVERDATA)"
+Write-Host "Fruit is $env:DRIVERDATA"
+Write-Host "Fruit is $env:FRUIT"
