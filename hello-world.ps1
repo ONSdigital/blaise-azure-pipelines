20a4f18c56734d70bb75318fe6010d4dd38dc601
Nik Webb <nik.webb@ons.gov.uk>
2021-01-21 14:04:28 +0000

    Pipeline (#1)
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Create hello-world.ps1
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Added in foundations for tests to run
    
    * Removed artifact name
    
    * Removed artifact name
    
    * Removed artifact name
    
    * Removed artifact name
    
    * Added drop to artifact name
    
    * Update hello-world.ps1
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update hello-world.ps1
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update hello-world.ps1
    
    * Update hello-world.ps1
    
    * testing variables
    
    * testing
    
    * testing
    
    * Update hello-world.ps1
    
    * Update hello-world.ps1
    
    * Update hello-world.ps1
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update InstallBlaise.ps1
    
    * Update InstallBlaise.ps1
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Changed to download pipeline artifact
    
    * Changed to download pipeline artifact
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Added additional job
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update hello-world.ps1
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * updated pipeline to branch
    
    * updated pipeline to branch
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Added runtime parameters

--- hello-world.ps1
+++ hello-world.ps1
@@ -1,1 +1,37 @@
 Write-Host "Hello Nik"
+
+function GetMetadataVariables1
+{
+  $variablesFromMetadata = Invoke-RestMethod http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=true -Headers @{ "Metadata-Flavor" = "Google" }
+  return $variablesFromMetadata | Get-Member -MemberType NoteProperty
+}
+
+function CreateVariables1($variableList)
+{
+
+  foreach ($variable in $variableList)
+  {
+      if ($variable.Name -Like "BLAISE_*")
+      {
+        $varName = $variable.Name
+        $varValue = $variable.Definition
+
+        $pattern = "^(.*?)$([regex]::Escape($varName) )(.?=)(.*)"
+
+         New-Variable -Scope script -Name ($varName) -Value ($varValue -replace $pattern, '$3')
+
+         $varValue = $varValue -replace $pattern, '$3'
+         Write-Host $varName '=' $varValue
+                 
+         #Write-Host "##vso[task.setvariable variable=$varName;]$varValue"
+        }
+  }
+}
+
+Write-Host "Setting up script and system variables..."
+$metadataVariables = GetMetadataVariables1
+CreateVariables1($metadataVariables)
+Write-Host "##vso[task.setvariable variable=sauce;]crushed tomatoes"
+Write-Host "Fruit is $env:DRIVERDATA"
+Write-Host "Non-secrets automatically mapped in, sauce is $env:SAUCE"
+
