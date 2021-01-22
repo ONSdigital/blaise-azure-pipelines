024b9a6a82916104457304897ce93cf2fc9a2fb0
Nik Webb <nik.webb@ons.gov.uk>
2021-01-22 16:00:23 +0000

    Added output variables

--- CheckBlaiseIsInstalled.ps1
+++ CheckBlaiseIsInstalled.ps1
@@ -1,10 +1,10 @@
 $app = Get-ItemProperty -Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"| Where-Object { $_.DisplayName -match 'blaise' } |Select-Object DisplayName, DisplayVersion, InstallDate, Version 
 
 if (!$app){
- Write-Host "##vso[task.setvariable variable=BlaiseInstalled]False"
+ Write-Host "##vso[task.setvariable variable=BlaiseInstalled;isOutput=true]False"
 }
 else
 {
-Write-Host "##vso[task.setvariable variable=BlaiseInstalled]True"
+Write-Host "##vso[task.setvariable variable=BlaiseInstalled;isOutput=true]True"
 }
 
