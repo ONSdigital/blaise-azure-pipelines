4363d5091d40ce01cda5162ef7e8f8163a637e35
Nik Webb <nik.webb@ons.gov.uk>
2021-01-26 14:18:46 +0000

    Update Create-A-Service.ps1

--- scripts/Create-A-Service.ps1
+++ scripts/Create-A-Service.ps1
@@ -5,7 +5,6 @@ sc.exe create $ServiceName binpath=C:\BlaiseServices\$ServiceName\$exeName.exe
 
 Write-Host "Adding Recovery options"
 sc.exe failure $ServiceName reset= 1 actions= restart/60000/run/1000
-sc.exe failure $ServiceName command= "\"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe\"Powershell -c C:\dev\stackdriver\logsServiceFailure.ps1 -ServiceName $ServiceName"
 
 Write-Host "Adding Delayed start to the service"
 sc.exe config $ServiceName start= delayed-auto
@@ -15,6 +14,3 @@ sc.exe start $ServiceName
 
 Write-Host "Adding Secondary recovery options"
 sc.exe failure $ServiceName reset= 7200 actions= restart/0/restart/30000//
-
-Write-Host "Disable StartService ScheduledTask"
-Get-ScheduledTask -TaskName  "StartService" | Enable-ScheduledTask
