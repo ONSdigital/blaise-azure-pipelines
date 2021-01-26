6f6392c289ec96d2889083e03c9843ad3fed1091
Nik Webb <nik.webb@ons.gov.uk>
2021-01-26 13:33:13 +0000

    Update Create-A-Service.ps1

--- scripts/Create-A-Service.ps1
+++ scripts/Create-A-Service.ps1
@@ -1,20 +1,20 @@
-param ($exeName, $BuildName)
+param ($exeName, $ServiceName)
 
 Write-Host "Creating Service"
-sc.exe create $BuildName binpath=C:\BlaiseServices\$BuildName\$exeName.exe
+sc.exe create $ServiceName binpath=C:\BlaiseServices\$ServiceName\$exeName.exe
 
 Write-Host "Adding Recovery options"
-sc.exe failure $BuildName reset= 1 actions= restart/60000/run/1000
-sc.exe failure $BuildName command= "\"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe\"Powershell -c C:\dev\stackdriver\logsServiceFailure.ps1 -ServiceName $BuildName"
+sc.exe failure $ServiceName reset= 1 actions= restart/60000/run/1000
+sc.exe failure $ServiceName command= "\"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe\"Powershell -c C:\dev\stackdriver\logsServiceFailure.ps1 -ServiceName $ServiceName"
 
 Write-Host "Adding Delayed start to the service"
-sc.exe config $BuildName start= delayed-auto
+sc.exe config $ServiceName start= delayed-auto
 
 Write-Host "Starting the service"
-sc.exe start $BuildName
+sc.exe start $ServiceName
 
 Write-Host "Adding Secondary recovery options"
-sc.exe failure $BuildName reset= 7200 actions= restart/0/restart/30000//
+sc.exe failure $ServiceName reset= 7200 actions= restart/0/restart/30000//
 
 Write-Host "Disable StartService ScheduledTask"
 Get-ScheduledTask -TaskName  "StartService" | Enable-ScheduledTask
