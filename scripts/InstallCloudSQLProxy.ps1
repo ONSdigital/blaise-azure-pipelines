6f416e0f6aaa256010c96a17fbf50ef650246926
Nik Webb <nik.webb@ons.gov.uk>
2021-01-27 12:22:50 +0000

    Update InstallCloudSQLProxy.ps1

--- scripts/InstallCloudSQLProxy.ps1
+++ scripts/InstallCloudSQLProxy.ps1
@@ -1,6 +1,9 @@
 Write-Host "Installing CloudSQL Proxy and NSSM Process"
+Write-Host "GCP Bucket" $env:BLAISE_GCP_BUCKET
+Write-Host "SQL Connect: " $env:ENV_CLOUDSQL_CONNECT
+
 gsutil cp "gs://$env:BLAISE_GCP_BUCKET/nssm.exe" "C:\Windows\nssm.exe"
 gsutil cp "gs://$env:BLAISE_GCP_BUCKET/cloud_sql_proxy_x64.exe" "C:\Windows\cloud_sql_proxy_x64.exe"
-nssm install cloudsql_proxy C:\Windows\cloud_sql_proxy_x64.exe -instances="$env:ENV_CLOUDSQL_CONNECT=tcp:3306" -ip_address_types=PRIVATE
-nssm set cloudsql_proxy Start SERVICE_AUTO_START
-nssm start cloudsql_proxy
+nssm.exe install cloudsql_proxy C:\Windows\cloud_sql_proxy_x64.exe -instances="$env:ENV_CLOUDSQL_CONNECT=tcp:3306" -ip_address_types=PRIVATE
+nssm.exe set cloudsql_proxy Start SERVICE_AUTO_START
+nssm.exe start cloudsql_proxy
