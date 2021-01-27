84d9f698d5ff818f47e036fa4db2df2531cce3ab
Jamie Kerr <jamiestephenkerr@gmail.com>
2021-01-27 14:32:05 +0000

    Added user role jason file and script

---
+++ scripts/UserRoles/CreateUserRoles.ps1
@@ -0,0 +1,11 @@
+﻿$userRolesUri = "$($env:BASE_URI_VAR)/roles/user"
+$rolesJsonFile = "userroles.json"
+
+$userRoles = Get-Content -Raw -Path $rolesJsonFile | ConvertFrom-Json 
+
+foreach ($userRole in $userRoles)
+{   
+    $body = $userRole | ConvertTo-Json 
+
+    Invoke-RestMethod -UseBasicParsing $userRolesUri -ContentType "application/json" -Method POST -Body $body
+}
