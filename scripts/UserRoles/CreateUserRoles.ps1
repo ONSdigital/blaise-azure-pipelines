de7b40362193777fd8c5b1284b8f1fadf7549cc5
James Williams <59868628+JAWilliams1990@users.noreply.github.com>
2021-01-28 11:07:22 +0000

    Update CreateUserRoles.ps1
    changed url..again

--- scripts/UserRoles/CreateUserRoles.ps1
+++ scripts/UserRoles/CreateUserRoles.ps1
@@ -1,5 +1,5 @@
 ﻿$userRolesUri = "$($env:ENV_RESTAPI_URL)/api/v1/users/roles"
-$rolesJsonFile = "/scripts/UserRoles/userroles.json"
+$rolesJsonFile = "scripts/UserRoles/userroles.json"
 
 $userRoles = Get-Content -Raw -Path $rolesJsonFile | ConvertFrom-Json 
 
