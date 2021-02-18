760dd3359e37dd3fb85cfe1db2276334075c1464
James Williams <59868628+JAWilliams1990@users.noreply.github.com>
2021-02-18 15:16:49 +0000

    Lu 7967 upgrade blaise (#12)
    * Added blaise version check
    * Add Upgrade Script
    * Updated upgrade script to use env variables from terraform
    * Added condition to smoke tests for upgrades
    
    Co-authored-by: Nik Webb <nik.webb@ons.gov.uk>

--- scripts/Blaise/CheckBlaiseIsInstalled.ps1
+++ scripts/Blaise/CheckBlaiseIsInstalled.ps1
@@ -5,6 +5,17 @@ if (!$app){
 }
 else
 {
+Write-Host "Blaise Version " + $app.DisplayVersion + " is installed"
 Write-Host "##vso[task.setvariable variable=BlaiseInstalled;isOutput=true]True"
+
+    if ($app.DisplayVersion -eq $env:ENV_BLAISE_CURRENT_VERSION)
+    {
+        Write-Host "Blaise is currently on the correct version"
+        Write-Host "##vso[task.setvariable variable=UpgradeBlaise;isOutput=true]False" 
+    }
+    else {
+        Write-Host "Blaise need to be upgraded to the latest version"
+        Write-Host "##vso[task.setvariable variable=UpgradeBlaise;isOutput=true]True"       
+    }
 }
 
