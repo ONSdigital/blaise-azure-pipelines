46721e2971149dc1c6f0fcac500233e320e96b90
Sam Bryant <sam.bryant@armakuni.com>
2021-02-19 09:32:10 +0000

    Use ENV_BLAISE_CATI_URL if set
    Defaults to ENV_BLAISE_EXTERNAL_SERVER_HOST_NAME if it is not set

--- scripts/SQL/CatiDatabaseUpdate.ps1
+++ scripts/SQL/CatiDatabaseUpdate.ps1
@@ -1,7 +1,12 @@
 try
 {
-    write-host "Updated Cati Database : $env:ENV_BLAISE_EXTERNAL_SERVER_HOST_NAME"
-    c:\dev\data\sqlite3 D:\Blaise5\Settings\ServerManagerDatabase.db "Update Server Set Name='$env:ENV_BLAISE_EXTERNAL_SERVER_HOST_NAME', Binding='https' where id=2;"
+    if (!$env:ENV_BLAISE_CATI_URL) {
+        $CATI_URL=$env:ENV_BLAISE_EXTERNAL_SERVER_HOST_NAME
+    } else {
+        $CATI_URL=$env:ENV_BLAISE_CATI_URL
+    }
+    write-host "Updated Cati Database : $CATI_URL"
+    c:\dev\data\sqlite3 D:\Blaise5\Settings\ServerManagerDatabase.db "Update Server Set Name='$CATI_URL', Binding='https' where id=2;"
     write-host "Cati Database updated"
 }
 catch {
