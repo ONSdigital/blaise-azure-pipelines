92e80797640093ae12dd338196373d1b09fd615e
Nik Webb <nik.webb@ons.gov.uk>
2021-02-22 11:16:24 +0000

    Updated logging message

--- scripts/SQL/CatiDatabaseUpdate.ps1
+++ scripts/SQL/CatiDatabaseUpdate.ps1
@@ -5,7 +5,7 @@ try
     } else {
         $CATI_URL=$env:ENV_BLAISE_CATI_URL
     }
-    write-host "Updated Cati Database : $CATI_URL"
+    write-host "Updating Cati Database : $CATI_URL"
     c:\dev\data\sqlite3 D:\Blaise5\Settings\ServerManagerDatabase.db "Update Server Set Name='$CATI_URL', Binding='https' where id=2;"
 }
 catch {
