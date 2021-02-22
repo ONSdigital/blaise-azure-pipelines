8491f68e70afd35d1c3f6b310a744ce9e1529e2c
Nik Webb <nik.webb@ons.gov.uk>
2021-02-22 10:50:35 +0000

    Throwing an error if the script fails

--- scripts/SQL/CatiDatabaseUpdate.ps1
+++ scripts/SQL/CatiDatabaseUpdate.ps1
@@ -7,9 +7,11 @@ try
     }
     write-host "Updated Cati Database : $CATI_URL"
     c:\dev\data\sqlite3 D:\Blaise5\Settings\ServerManagerDatabase.db "Update Server Set Name='$CATI_URL', Binding='https' where id=2;"
-    write-host "Cati Database updated"
 }
 catch {
     Write-Host "Cati Database Update has failed"
     Write-Host $_
+    exit 1
 }
+write-host "Cati Database updated"
+
