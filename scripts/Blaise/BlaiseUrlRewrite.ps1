1369d7f7d8fbd0546e260e3f65de7c005034440a
Nik Webb <nik.webb@ons.gov.uk>
2021-02-26 09:04:58 +0000

    Updated rewrite rules script

--- scripts/Blaise/BlaiseUrlRewrite.ps1
+++ scripts/Blaise/BlaiseUrlRewrite.ps1
@@ -5,11 +5,28 @@ $siteName = "Blaise"
 $ruleName = "Blaise data entry"
 $serverName = "https://$env:ENV_BLAISE_CATI_URL"
 
-Write-Host "Adding rewrite rule"
+$existing = Get-WebConfigurationProperty -pspath "iis:\sites\Default Web Site\$siteName" -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']" -Name "."
 
-Add-WebConfigurationProperty -pspath "iis:\sites\Default Web Site\$siteName" -filter "system.webServer/rewrite/outboundrules" -name "." -value @{name=$ruleName}
-Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/match" -name "pattern" -value "http://$env:ENV_BLAISE_SERVER_HOST_NAME"
-Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "type" -value "Rewrite"
-Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "value" -value "$serverName"
+if ($existing){
+    Write-Host "Re write rule already exists."
+    exit 0
+}
 
-Write-Host "Rewrite rules applied"
+try{
+    Write-Host "Adding rewrite rule"
+
+    Add-WebConfigurationProperty -pspath "iis:\sites\Default Web Site\$siteName" -filter "system.webServer/rewrite/outboundrules" -name "." -value @{name=$ruleName}
+    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/match" -name "pattern" -value "http://$env:ENV_BLAISE_SERVER_HOST_NAME"
+    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "type" -value "Rewrite"
+    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "value" -value "$serverName"
+
+    Write-Host "Rewrite rules applied"
+}
+catch
+{
+    Write-Host $_.ScriptStackTrace
+    Write-Host "Rewrite rules have not been applied"
+    exit 1
+}
+
+
