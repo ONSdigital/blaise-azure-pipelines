8c6d6e3778bf299cb6c088ebf276cdd6e59645d2
James Williams <williamsja9@gmail.com>
2021-03-01 15:44:14 +0000

    added in session time out scripts

---
+++ scripts/Blaise/IncreaseIISTimeout.ps1
@@ -0,0 +1,21 @@
+try{
+    Write-Host "Setting session state time-out"
+    Set-WebConfigurationProperty system.web/sessionState "IIS:\Sites\Default Web Site\Blaise" -Name "Timeout" -Value:08:00:00
+    Write-Host "Session time-out set"
+}
+catch{
+    Write-Host "Could not set session state time-out"
+    Write-Host $_.ScriptStackTrace
+    exit 1
+}
+
+Try{
+    Write-Host "Setting ISS idle time-out"
+    Set-ItemProperty ("IIS:\AppPools\BlaiseAppPool") -Name processModel.idleTimeout -value ( [TimeSpan]::FromMinutes(480))
+    Write-Host "ISS idle set"
+}
+catch{
+    Write-Host "Could not set ISS idle time-out"
+    Write-Host $_.ScriptStackTrace
+    exit 1
+}
