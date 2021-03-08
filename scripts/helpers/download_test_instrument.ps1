06e73ee1f2a121c6987c485c36e4a7a9259125d1
Nik Webb <nik.webb@ons.gov.uk>
2021-03-08 09:51:30 +0000

    Add variables template Updated scripts to use variables

--- scripts/helpers/download_test_instrument.ps1
+++ scripts/helpers/download_test_instrument.ps1
@@ -2,4 +2,4 @@ Write-Host "Login to GCP"
 gcloud auth activate-service-account $env:ENV_SHARED_SERVICE_ACCOUNT --key-file=$(gcpkey.secureFilePath)
 
 Write-Host "Downloading instrument"
-gsutil cp gs://$env:ENV_SHARED_BUCKET/$env:InstrumentName.bpkg c:\survey\$env:InstrumentName.bpkg
+gsutil cp gs://$env:ENV_SHARED_BUCKET/$env:InstrumentName.$env:PACKAGE_EXTENSION $env:InstrumentPath\$env:InstrumentName.bpkg
