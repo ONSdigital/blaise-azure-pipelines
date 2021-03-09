18b4c6fab6580557734005e6423743eac23e29d5
James Williams <williamsja9@gmail.com>
2021-03-09 16:48:23 +0000

    Nik is a saint - here goes

---
+++ scripts/UserRoles/CheckUserRolesExist.ps1
@@ -0,0 +1,27 @@
+$userRolesUri = "$env:ENV_RESTAPI_URL/api/v1/userroles"
+$rolesJsonFile = "scripts/UserRoles/userroles.json"
+
+$userRoles = Get-Content -Raw -Path $rolesJsonFile | ConvertFrom-Json 
+
+foreach ($userRole in $userRoles)
+{   
+    $exists =  Invoke-RestMethod -UseBasicParsing "$($userRolesUri)/$($userRole.name)/exists" -ContentType "application/json" -Method GET
+    
+    If ($exists -ne $true) {
+        Write-Host "User roles does not exist. Creating user roles"
+        Invoke-Expression "$currentPath\scripts\UserRoles\CreateUserRoles.ps1"
+        exit 
+    }
+
+    $response = Invoke-RestMethod -UseBasicParsing "$($userRolesUri)/$($userRole.name)" -ContentType "application/json" -Method GET
+    
+    $roleEqual = ($response | ConvertTo-Json -Compress) -eq 
+                 ($userRole | ConvertTo-Json -Compress)
+
+    If ($roleEqual -ne $true) {
+        Write-Host "User role permissions do not exist. Creating user role permissons"
+        Invoke-Expression "$currentPath\scripts\UserRoles\CreateUserRoles.ps1"
+        exit
+    }
+}
+Write-Host "User roles already exist"
