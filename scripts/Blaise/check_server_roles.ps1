97cd669d33b3ae3d71d7433a9861c1536ee162d0
Nik Webb <nik.webb@ons.gov.uk>
2021-03-31 20:47:25 +0100

    Added check for node roles

---
+++ scripts/Blaise/check_server_roles.ps1
@@ -0,0 +1,52 @@
+function NodeHasTheCorrectRoles {
+    $requiredRoles = RolesNodeShouldHave
+    $currentRoles = ParseCurrentNodeRoles
+    return CheckNodeHasCorrectRoles -CurrentNodeRoles $currentRoles -RolesNodeShouldHave $requiredRoles
+}
+function RolesNodeShouldHave {
+    $rolesItShouldHave = $env:ENV_BLAISE_ROLES.Trim().Split(',') | Sort-Object
+    return $rolesItShouldHave -join ','
+}
+
+function CurrentNodeRoles {
+    return C:\Blaise5\Bin\ServerManager -lsr | Out-String
+}
+
+function ParseCurrentNodeRoles {
+    param (
+        [string] $CurrentRoles
+    )
+    if ([string]::IsNullOrEmpty($CurrentRoles))
+    {
+        $CurrentRoles = CurrentNodeRoles
+    }
+    $roles = ""
+
+    Switch -regex ($CurrentRoles)
+    {
+        'ADMIN' { $roles += 'admin,' }
+        'CATI' { $roles += 'cati,' }
+        'AUDITTRAIL' { $roles += 'audittrail,' }
+        'WEB' { $roles += 'web,' }
+        'SESSION' { $roles += 'session,' }
+        'DATA' { $roles += 'data,' }
+        'RESOURCE' { $roles += 'resource,' }
+        'DATAENTRY' { $roles += 'dataentry,' }
+        'DASHBOARD' { $roles += 'dashboard' }
+    }
+
+    $roles = $roles.Trim().Split(',') | Sort-Object
+    return $roles -join ','
+}
+
+function CheckNodeHasCorrectRoles {
+    param (
+        [string] $CurrentNodeRoles,
+        [string] $RolesNodeShouldHave
+    )
+    return $CurrentNodeRoles -eq $RolesNodeShouldHave
+}
+
+
+
+
