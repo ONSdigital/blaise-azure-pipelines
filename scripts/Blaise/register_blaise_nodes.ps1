fffbe65f50eece0399db244010206ff3303ed896
Nik Webb <nik.webb@ons.gov.uk>
2021-03-31 10:01:19 +0100

    Added register node script

---
+++ scripts/Blaise/register_blaise_nodes.ps1
@@ -0,0 +1,46 @@
+function RegisterNode{
+    param(
+        [string] $CurrentNode = $env:computername,
+        [string] $ManagementNode = $env:ENV_BLAISE_SERVER_HOST_NAME,
+        [string] $ConnectionPort = $env:ENV_BLAISE_CONNECTION_PORT,
+        [securestring] $BlaisePassword = $env:ENV_BLAISE_ADMIN_PASSWORD,
+        [string] $BlaiseServerPark = $env:ENV_BLAISE_SERVER_PARK_NAME,
+        [string] $BlaiseUserName = $env:ENV_BLAISE_ADMIN_USER
+    )
+
+    If ([string]::IsNullOrEmpty($ManagementNode)) {
+        throw [System.IO.ArgumentException] "No management node argument provided"
+    }
+
+    If ([string]::IsNullOrEmpty($ConnectionPort)) {
+        throw [System.IO.ArgumentException] "No Blaise connection port argument provided"
+    }
+
+    If ([string]::IsNullOrEmpty($BlaisePassword)) {
+        throw [System.IO.ArgumentException] "No Blaise Admin Password argument provided"
+    }
+
+    If ([string]::IsNullOrEmpty($BlaiseServerPark)) {
+        throw [System.IO.ArgumentException] "No Blaise server park argument provided"
+    }
+
+    If ([string]::IsNullOrEmpty($BlaiseUserName)) {
+        throw [System.IO.ArgumentException] "No Blaise username argument provided"
+    }
+
+    Write-Host "Registering $currentNode on management node $managementNode"
+
+    c:\blaise5\bin\servermanager -addserverparkserver:$currentNode -server:$managementNode 
+    -binding:http -port:$connectionPort -user:$blaiseUserName -password:$blaisePassword -serverpark:$blaiseServerPark -serverport:$connectionPort 
+    -serverbinding:http -masterhostname:$managementNode -logicalroot:default -server:$managementNode -binding:http -port:$connectionPort
+
+    Write-Host "$currentNode registered"
+}
+
+try{
+    RegisterNode
+}
+catch{
+    Write-Host "Nodes have not been registered: $($_.ScriptStackTrace)"
+    exit 1
+}
