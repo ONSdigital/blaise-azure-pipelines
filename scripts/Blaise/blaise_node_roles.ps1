d53bf9c15322ceb9ea0357595dbb689441ad40a0
Nik Webb <nik.webb@ons.gov.uk>
2021-03-31 20:48:11 +0100

    Added functions to set blaise nodes to the correct role

---
+++ scripts/Blaise/blaise_node_roles.ps1
@@ -0,0 +1,25 @@
+. "$PSScriptRoot\check_server_roles.ps1"
+. "$PSScriptRoot\..\check_server_roles.ps1"
+
+try {
+    if (NodeHasTheCorrectRoles)
+    {
+        $roles = RolesNodeShouldHave
+
+        Write-Host "Updating node roles to: $roles"
+        c:\blaise5\bin servermanager -role:$roles
+        Write-Host "Node roles updated"
+
+        Write-Host "Restarting Blaise services"
+        sc.exe stop blaise5services 
+        sc.exe start blaise5services
+        Write-Host "Blaise has been restarted"
+    }
+    else {
+        Write-Host "Node has the correct roles"
+    }
+}
+catch {
+    Write-Host "Error updating blaise node roles. $($_.ScriptStackTrace)"
+    exit 1
+}
