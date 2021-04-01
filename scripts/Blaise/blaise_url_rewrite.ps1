df49013c1a49daba6c92824e5f7bfbdf86c73796
Nik Webb <nik.webb@ons.gov.uk>
2021-04-01 16:36:02 +0100

    Updated rewrite rules for data entry

--- scripts/Blaise/blaise_url_rewrite.ps1
+++ scripts/Blaise/blaise_url_rewrite.ps1
@@ -16,7 +16,7 @@ try{
     Write-Host "Adding rewrite rule"
 
     Add-WebConfigurationProperty -pspath "iis:\sites\Default Web Site\$siteName" -filter "system.webServer/rewrite/outboundrules" -name "." -value @{name=$ruleName}
-    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/match" -name "pattern" -value "http://$env:ENV_BLAISE_SERVER_HOST_NAME"
+    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/match" -name "pattern" -value "http://blaise-gusty-data[^/]*"
     Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "type" -value "Rewrite"
     Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "value" -value "$serverName"
 
