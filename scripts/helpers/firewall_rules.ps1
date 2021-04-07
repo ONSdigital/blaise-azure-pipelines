414ea63576dc4b754d9d048d516027c2064015b1
Nik Webb <nik.webb@ons.gov.uk>
2021-04-07 20:38:29 +0100

    wip

--- scripts/helpers/firewall_rules.ps1
+++ scripts/helpers/firewall_rules.ps1
@@ -1,5 +1,6 @@
 param ($inbound_ports, $outbound_ports)
 
+Write-Host "Inbound ports $inbound_ports, outbound ports $outbound_ports"
 New-NetFirewallRule -DisplayName "Blaise" -Direction Inbound -LocalPort 80, 443, $inbound_ports -Protocol TCP -Action Allow
 New-NetFirewallRule -DisplayName "Blaise" -Direction Outbound -RemotePort 80, 443, $outbound_ports -Protocol TCP -Action Allow
 
