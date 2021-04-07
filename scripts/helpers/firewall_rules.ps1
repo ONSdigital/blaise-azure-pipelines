12da4dc98f7bed7944d03422c02b2fa10eeb8a09
Nik Webb <nik.webb@ons.gov.uk>
2021-04-07 21:40:59 +0100

    added split join function to powershell

--- scripts/helpers/firewall_rules.ps1
+++ scripts/helpers/firewall_rules.ps1
@@ -1,6 +1,9 @@
 param ($inbound_ports, $outbound_ports)
 
-Write-Host "Inbound ports $inbound_ports, outbound ports $outbound_ports"
-New-NetFirewallRule -DisplayName "Blaise" -Direction Inbound -LocalPort 80, 443, $inbound_ports -Protocol TCP -Action Allow
-New-NetFirewallRule -DisplayName "Blaise" -Direction Outbound -RemotePort 80, 443, $outbound_ports -Protocol TCP -Action Allow
+$InboundPorts = $inbound_ports -split ' ' -join ','
+$OutboundPorts = $outbound_ports -split ' ' -join ','
+
+Write-Host "Inbound ports $InboundPorts, outbound ports $OutboundPorts"
+New-NetFirewallRule -DisplayName "Blaise" -Direction Inbound -LocalPort 80, 443, $InboundPorts -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Blaise" -Direction Outbound -RemotePort 80, 443, $OutboundPorts -Protocol TCP -Action Allow
 
