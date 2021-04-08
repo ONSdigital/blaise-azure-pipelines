84c1a2d499530d311bbedbe85c883cb14bc62920
Nik Webb <nik.webb@ons.gov.uk>
2021-04-08 13:26:00 +0100

    Updated the firewall script

--- scripts/helpers/firewall_rules.ps1
+++ scripts/helpers/firewall_rules.ps1
@@ -1,9 +1,71 @@
-param ($inbound_ports, $outbound_ports)
+param ([string]$RuleName, [string[]]$Inbound_Ports, [string[]]$Outbound_Ports)
 
-$InboundPorts = $inbound_ports -split ' ' -join ', '
-$OutboundPorts = $outbound_ports -split ' ' -join ', '
+function SetFirewallRules
+{
+    param (
+        [string]$RuleName, 
+        [string[]] $Ports, 
+        [string] $Direction,
+        [string] $PortType
+    )
+    if ($PortType -eq "LocalPort")
+    {
+        New-NetFirewallRule -DisplayName $RuleName -Direction $Direction -LocalPort $Ports -Protocol TCP -Action Allow
+        Write-Host "$RuleName $Direction firewall rule created with the following Local ports: $Ports"
+    }
+    if ($PortType -eq "RemotePort")
+    {
+        New-NetFirewallRule -DisplayName $RuleName -Direction $Direction -RemotePort $Ports -Protocol TCP -Action Allow
+        Write-Host "$RuleName $Direction firewall rule created with the following Remote ports: $Ports"
+    }
+}
 
-Write-Host "Inbound ports $InboundPorts, outbound ports $OutboundPorts"
-New-NetFirewallRule -DisplayName "Blaise" -Direction Inbound -LocalPort 80, 443, $InboundPorts -Protocol TCP -Action Allow
-New-NetFirewallRule -DisplayName "Blaise" -Direction Outbound -RemotePort 80, 443, $OutboundPorts -Protocol TCP -Action Allow
+function PortsMatch {
+    param (
+        [string]$RuleName,
+        [string[]] $Ports,
+        [string] $Direction,
+        [string] $PortType
+    )
+    Get-NetFirewallRule -DisplayName $RuleName | Where-Object -Property Direction -EQ $Direction | Get-NetFirewallPortFilter | ForEach-Object{
+            
+        if ([string]$_.$PortType -eq [string]$Ports)
+        {
+            return $true
+        }
+        else
+        {
+            return $false
+        }
+    }
+}
 
+try{
+    if (Get-NetFirewallRule -DisplayName "$RuleName")
+    {
+        $LocalPortExists = PortsMatch -RuleName:"$RuleName" -Ports:$Inbound_Ports -Direction:"Inbound" -PortType:"LocalPort"
+        $RemotePortExists = PortsMatch -RuleName:"$RuleName" -Ports:$Outbound_Ports -Direction:"Outbound" -PortType:"RemotePort"
+            
+        if (-Not $RemotePortExists -or -Not $LocalPortExists)
+        {
+            Remove-NetFirewallRule -DisplayName "$RuleName"
+            SetFirewallRules -RuleName:"$RuleName" -Direction:"Inbound" -Ports:$Inbound_Ports -PortType:"LocalPort"   
+            SetFirewallRules -RuleName:"$RuleName" -Direction:"Outbound" -Ports:$Outbound_Ports -PortType:"RemotePort"
+        }
+        else
+        {
+            Write-Host "Firewall rules already exist"
+        }
+    }
+    else 
+    {
+        Write-Host "No Firewall rule exists, Creating them"
+        SetFirewallRules -RuleName:"$RuleName" -Direction:"Inbound" -Ports:$Inbound_Ports -PortType:"LocalPort"
+        SetFirewallRules -RuleName:"$RuleName" -Direction:"Outbound" -Ports:$Outbound_Ports -PortType:"RemotePort"
+    }
+}
+catch{
+    Write-Host "Unable to create firewall rules"
+    Write-Host $_.ScriptStackTrace
+    exit 1
+}
