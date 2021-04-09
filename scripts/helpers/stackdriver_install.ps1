5697f6e95761abf10e5444272e91ec8d4a0b9e98
Nik Webb <nik.webb@ons.gov.uk>
2021-04-09 14:10:02 +0100

    Added stackdriver to pipeline

---
+++ scripts/helpers/stackdriver_install.ps1
@@ -0,0 +1,60 @@
+param ([string]$MonitoringExe, [string]$LoggingExe, [string]$FolderPath)
+
+function DownloadFileIfItDoesntExist {
+    param ([string] $FileName, [string]$FolderPath)
+    if (!(Test-Path "$FolderPath\$FileName" -PathType Leaf))
+    {
+    $env:ENV_BLAISE_GCP_BUCKET = "ons-blaise-v2-dev-multi-winvm-data"
+    Write-Host "Downloading $FileName..."
+    gsutil cp gs://$env:ENV_BLAISE_GCP_BUCKET/$FileName $FolderPath\$FileName
+    }
+}
+
+function StartServiceIfStopped {
+    param (
+        [string] $ServiceName
+    )
+    If ((Get-Service $ServiceName).Status -eq 'Stopped') 
+    {
+        Start-Service $ServiceName
+        Write-Host "Starting $ServiceName"
+    }
+    Else {
+        Write-Host "$ServiceName found and running nothing to do..."
+    }
+}
+
+New-Item -Path "$FolderPath" -ItemType "directory" -Force
+
+#############
+# MONITORING
+#############
+$monitor_service = "StackdriverMonitoring"
+
+If (Get-Service $monitor_service -ErrorAction SilentlyContinue) 
+{
+    StartServiceIfStopped -ServiceName $monitor_service
+}
+Else {
+    DownloadFileIfItDoesntExist -FileName:$MonitoringExe -FolderPath $FolderPath
+
+    Write-Host "Installing Stackdriver monitoring agent..."
+    & $FolderPath\$MonitoringExe /S /D="$FolderPath\MonitoringAgent"
+}
+
+############
+# LOGGING
+############
+
+$log_service = "StackdriverLogging"
+
+If (Get-Service $log_service -ErrorAction SilentlyContinue) 
+{
+    StartServiceIfStopped -ServiceName $log_service
+}
+Else {
+    DownloadFileIfItDoesntExist -FileName:$LoggingExe -FolderPath $FolderPath
+
+    Write-Host "Installing Stackdriver Logging agents..."
+    & $FolderPath\$LoggingExe /S /D="$FolderPath\LoggingAgent"
+}
