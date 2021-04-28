432936dfd0d5cd7f20d70fe625944bf72a58ef79
Nik Webb <nik.webb@ons.gov.uk>
2021-04-28 11:45:57 +0100

    Multinode to dev (#24)
    * split out blaise management steps
    
    * renamed jobs
    
    * Added dependency to management setup
    
    * Added register node script
    
    * Added check for node roles
    
    * Added functions to set blaise nodes to the correct role
    
    * Added missing steps syntax
    
    * added register node template
    
    * Removed unused reference
    
    * Updated display names
    
    * Moved script to one line
    
    * removed secure string
    
    * Updated service names
    
    * added missing escape characters
    
    * changed restart service
    
    * updated password variable
    
    * Updated rewrite rules for data entry
    
    * Added blaise firewall to the pipeline
    
    * Updating display names
    
    * opps
    
    * updated file path for firewall rules
    
    * wip
    
    * updated ports
    
    * changed quotes
    
    * changed parameter input
    
    * adding a comma
    
    * removed the quotes
    
    * Changed the way variables are added to parameters
    
    * testing
    
    * hard coded values
    
    * maybe
    
    * added split join function to powershell
    
    * added space to the join
    
    * Updated the firewall script
    
    * tweak
    
    * removed try catch
    
    * Added error action
    
    * Updated ports
    
    * Changed firewall yaml
    
    * Added stackdriver to pipeline
    
    * added error action
    
    * Removed error action
    
    * Improved output messages
    
    * Trying harded install
    
    * removed logging
    
    * Install using powershell wait
    
    * updated variables
    
    * updated exe
    
    * MonitoringExe
    
    * removed variable install
    
    * wip
    
    * wip
    
    * added sleep
    
    * removed destination path
    
    * testing
    
    * Removed stackdriver from pipeline
    
    * added multi node variable
    
    * Added conditions to multinode steps
    
    * removed unused parameter
    
    * moved condition to the template
    
    * Updated variables for multi node
    
    * removed rest api url injection
    
    * Scrapping meta data from vms every time we check if roles need to be updated
    
    * updated exception error message
    
    * Force variables to overwrite if they already exist
    
    * Updated environment variable to match metadata
    
    * updated script to get metadata
    
    * added try catch to triming of variables
    
    * Scrapped ENV vars from metadata
    
    * added helper to update environment variables
    
    * corrected file path
    
    * wait for Blaise to update server roles before restarting
    
    * updated RolesNodeShouldHave
    
    Co-authored-by: elthorne <46898565+elthorne@users.noreply.github.com>

--- scripts/Blaise/upgrade_blaise.ps1
+++ scripts/Blaise/upgrade_blaise.ps1
@@ -18,13 +18,13 @@ Start-Process -Wait "msiexec" -ArgumentList $blaise_args
 Write-Host "Blaise uninstall complete"
 
 Write-Host "Blaise admin user: $env:ENV_BLAISE_ADMIN_USER"
-Write-Host "Blaise admin password: $env:ENV_BLAISE_ADMIN_PASS"
+Write-Host "Blaise admin password: $env:ENV_BLAISE_ADMIN_PASSWORD"
 Write-Host "Setting Blaise upgrade args"
 $blaise_args = "/qn","/norestart","/log upgrade.log","/i C:\dev\data\Blaise\Blaise5.msi"
 $blaise_args += "FORCEINSTALL=1"
 $blaise_args += "INSTALLATIONMODE=Upgrade"
 $blaise_args += "ADMINISTRATORUSER=$env:ENV_BLAISE_ADMIN_USER"
-$blaise_args += "ADMINISTRATORPASSWORD=$env:ENV_BLAISE_ADMIN_PASS"
+$blaise_args += "ADMINISTRATORPASSWORD=$env:ENV_BLAISE_ADMIN_PASSWORD"
 Write-Host "blaise_args: $blaise_args"
 Write-Host "Running msiexec for Blaise upgrade"
 Start-Process -Wait "msiexec" -ArgumentList $blaise_args
