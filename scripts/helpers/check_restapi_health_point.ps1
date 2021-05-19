354723d3d74ad71880e72af05c8fcef06e4cf7ff
Nik Webb <nik.webb@ons.gov.uk>
2021-05-19 14:47:07 +0100

    added health check test

---
+++ scripts/helpers/check_restapi_health_point.ps1
@@ -0,0 +1,18 @@
+try {
+    $response = Invoke-WebRequest $env:TESTING_URL -Method Get
+    $hostname = hostname
+    $response.StatusCode
+
+    if ($response.StatusCode -eq 200)
+    {
+        Write-Host "$hostname responded with status code 200"
+    }
+    else
+    {
+        Write-Host "$hostname has issues: $($response.StatusCode)"
+        exit 1
+    }
+}
+catch {
+    exit 1
+}
