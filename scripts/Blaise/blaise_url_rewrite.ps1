9ed42a88c427f8e421922d8387c8e2cbdcf29bbe
Nik Webb <nik.webb@ons.gov.uk>
2021-05-19 13:02:07 +0100

    Rewrite rule single node (#27)
    * added mgmt node rewrite rule for single node mode
    
    * updated outputs
    
    * improved exception message
    
    * added return to add rewrite rules function
    
    * updated rule for mgmt node
    
    * Check to see if CATI DB is already registered
    
    * inverted test path logic

--- scripts/Blaise/blaise_url_rewrite.ps1
+++ scripts/Blaise/blaise_url_rewrite.ps1
@@ -10,35 +10,44 @@ function CheckIfURLRewriteMsiExists {
     }    
 }
 
+function AddRewriteRule {
+  param (
+    [string] $siteName,
+    [string] $ruleName,
+    [string] $serverName = "https://$env:ENV_BLAISE_CATI_URL",
+    [string] $rule
+  )
+  $existing = Get-WebConfigurationProperty -pspath "iis:\sites\Default Web Site\$siteName" -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']" -Name "."
+
+  if ($existing){
+      Write-Host "$ruleName already exists."
+      return
+  }
+
+  try{
+    Write-Host "Adding rewrite rule"
+
+    Add-WebConfigurationProperty -pspath "iis:\sites\Default Web Site\$siteName" -filter "system.webServer/rewrite/outboundrules" -name "." -value @{name=$ruleName}
+    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/match" -name "pattern" -value "$rule"
+    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "type" -value "Rewrite"
+    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "value" -value "$serverName"
+
+    Write-Host "$ruleName applied"
+  }
+  catch{
+      Write-Host $_.Exception.Message
+      Write-Host "Rewrite rules have not been applied"
+      exit 1
+  }
+}
+
 CheckIfURLRewriteMsiExists
 Write-Host "Install write url msi"
 Start-Process msiexec.exe -Wait -ArgumentList '/I C:\dev\data\rewrite_url.msi /quiet'
 
-$siteName = "Blaise"
-$ruleName = "Blaise data entry"
-$serverName = "https://$env:ENV_BLAISE_CATI_URL"
+AddRewriteRule -siteName "Blaise" -ruleName "Blaise data entry" -serverName "https://$env:ENV_BLAISE_CATI_URL" -rule "http://blaise-gusty-data[^/]*"
+AddRewriteRule -siteName "Blaise" -ruleName "Blaise mgmt" -serverName "https://$env:ENV_BLAISE_CATI_URL" -rule "http://blaise-gusty-mgmt*"
 
-$existing = Get-WebConfigurationProperty -pspath "iis:\sites\Default Web Site\$siteName" -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']" -Name "."
 
-if ($existing){
-    Write-Host "Re write rule already exists."
-    exit 0
-}
-
-try{
-    Write-Host "Adding rewrite rule"
-
-    Add-WebConfigurationProperty -pspath "iis:\sites\Default Web Site\$siteName" -filter "system.webServer/rewrite/outboundrules" -name "." -value @{name=$ruleName}
-    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/match" -name "pattern" -value "http://blaise-gusty-data[^/]*"
-    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "type" -value "Rewrite"
-    Set-WebConfigurationProperty -pspath "MACHINE/WEBROOT/APPHOST/Default Web Site/$siteName"  -filter "system.webServer/rewrite/outboundRules/rule[@name='$ruleName']/action" -name "value" -value "$serverName"
-
-    Write-Host "Rewrite rules applied"
-}
-catch{
-    Write-Host $_.ScriptStackTrace
-    Write-Host "Rewrite rules have not been applied"
-    exit 1
-}
 
 
--- scripts/Blaise/cati_mysql_database.ps1
+++ scripts/Blaise/cati_mysql_database.ps1
@@ -2,14 +2,29 @@
 
 try {
     $filePath = "D:\Blaise5\Settings\catidb.bcdi"
-    #Create data interface
-    CreateDataInterfaceFile -applicationType cati -filePath $filePath
-    Write-Host "Created Cati Data Interface File"
+    if (Test-Path $filePath)
+    {
+        Write-Host "$filePath already exists"  
+    }
+    else {
+        #Create data interface
+        CreateDataInterfaceFile -applicationType cati -filePath $filePath
+        Write-Host "Created Cati Data Interface File"
+    }
+    
+    #Get a list of all configuration settings for Blaise
+    $configurationSettings = ListOfConfigurationSettings
 
-    #register data interface
-    RegisterCatiDataInterfaceFile -filePath $filePath
-    restart-service blaiseservices5
-    Write-Host ".bcdi file registered"
+    if ($configurationSettings.contains($filePath))
+    {
+        Write-Host "$filePath is already registered"
+    }
+    else {
+        #register data interface
+        RegisterCatiDataInterfaceFile -filePath $filePath
+        restart-service blaiseservices5
+        Write-Host "$filePath registered"
+    }
 }
 catch {
     Write-Host "Error occured updated Cati database to mysql: $($_.Exception.Message) at: $($_.ScriptStackTrace)"
