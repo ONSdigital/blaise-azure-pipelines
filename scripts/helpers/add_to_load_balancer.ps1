4201d023543d95c0e3246978884dddb3d75c5b61
Nik Webb <nik.webb@ons.gov.uk>
2021-05-19 09:56:33 +0100

    seperated scripts into seperate files

---
+++ scripts/helpers/add_to_load_balancer.ps1
@@ -0,0 +1,10 @@
+param ($instancegroup, $zone)
+
+try {
+    gcloud compute instance-groups unmanaged add-instances $instancegroup --instances=$hostname --zone $zone
+    Write-Host "added $hostname to the $instancegroup group"
+}
+catch {
+    Write-Host "Unable to add $hostname to the instance group"
+    exit 1
+}
