92cb09ca1e4b1f6f73e29a8d43a453c66fc20716
Nik Webb <nik.webb@ons.gov.uk>
2021-05-24 11:57:59 +0100

    Added check to make sure instance is in the group before removing

--- scripts/helpers/remove_from_instance_group.ps1
+++ scripts/helpers/remove_from_instance_group.ps1
@@ -2,8 +2,14 @@ param ($instancegroup, $zone)
 
     try {
         $hostname = hostname
-        gcloud compute instance-groups unmanaged remove-instances $instancegroup --instances=$hostname --zone $zone
-        Write-Host "removed $hostname from $instancegroup group"
+
+        $instances = gcloud compute instance-groups unmanaged list-instances $instancegroup --zone $zone | Out-String
+
+        if ($instances.Contains($hostname))
+        {
+            gcloud compute instance-groups unmanaged remove-instances $instancegroup --instances=$hostname --zone $zone
+            Write-Host "removed $hostname from $instancegroup group"
+        }       
     }
     catch {
         Write-Host "Unable to remove $hostname from the instance group"
