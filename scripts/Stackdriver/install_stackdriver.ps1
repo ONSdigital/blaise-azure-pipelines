7cf9851edc425d21c2012aa81385e396c802089c
Nik Webb <nik.webb@ons.gov.uk>
2021-05-27 15:15:31 +0100

    encapsulate variables

--- scripts/Stackdriver/install_stackdriver.ps1
+++ scripts/Stackdriver/install_stackdriver.ps1
@@ -1,16 +1,16 @@
 param ([string]$loggingagent, [string]$monitoringagent, [string]$GCP_BUCKET)
 
-Write-Host "Target logging agent is: $loggingagent"
-Write-Host "Target monitoring agent is: $monitoringagent"
-Write-Host "GCP artifact bucket is: $GCP_BUCKET"
+Write-Host "Target logging agent is: $($loggingagent)"
+Write-Host "Target monitoring agent is: $($monitoringagent)"
+Write-Host "GCP artifact bucket is: $($GCP_BUCKET)"
 
 Write-Host "Checking if target logging agent version has been installed already..."
-if (Test-Path C:\dev\stackdriver\$loggingagent) {
+if (Test-Path C:\dev\stackdriver\$($loggingagent)) {
     Write-Host "Version already installed, skipping..."
 }
 else {
     Write-Host "Downloading Stackdriver logging agent installer from '$GCP_BUCKET'..."
-    gsutil cp gs://$GCP_BUCKET/$loggingagent "C:\dev\data\$loggingagent"
+    gsutil cp gs://$GCP_BUCKET/$loggingagent "C:\dev\data\$($loggingagent)"
 
     Write-Host "Installing Stackdriver logging agent..."
     C:\dev\stackdriver\$loggingagent /S /D="C:\dev\stackdriver\loggingAgent"
@@ -21,8 +21,8 @@ if (Test-Path C:\dev\stackdriver\$monitoringagent) {
     Write-Host "Version already installed, skipping..."
 }
 else {
-    Write-Host "Downloading Stackdriver monitoring agent installer from '$GCP_BUCKET'..."
-    gsutil cp gs://$GCP_BUCKET/$monitoringagent "C:\dev\data\$monitoringagent"
+    Write-Host "Downloading Stackdriver monitoring agent installer from '$($GCP_BUCKET)'..."
+    gsutil cp gs://$GCP_BUCKET/$monitoringagent "C:\dev\data\$($monitoringagent)"
 
     Write-Host "Installing Stackdriver monitoring agent..."
     C:\dev\stackdriver\$monitoringagent /S /D="C:\dev\stackdriver\monitoringAgent"
