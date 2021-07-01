cf366645dd0fae397c300d39c18ac192c32b0d00
Alistair Grew <Ali.Grew@cloudsolutions.co.uk>
2021-07-01 15:36:03 +0100

    Google Ops Agent (#37)
    * New code to do ops agent
    
    * Make it clearer feedback clearer
    
    * Update service checking logic
    
    * Checking to see if agent needs update
    
    * Add upgrade function
    
    * Missing }
    
    * Make the erorr messages a little nicer
    
    * Add parameter to install job
    
    * Automate the uninstall of old monitoring agent
    
    * Fix typo
    
    * Try running it in powershell core
    
    * Remove user profile stuff
    
    * Try a different execution method
    
    * Change flag positioning
    
    * Add verbose flags
    
    * Reduce number of functions
    
    * Set an error action preference
    
    * Should be a stop service
    
    * Move erroractionpreference above google code exec
    
    * Don't try to start a stopped service
    
    * Take out error condition on service not started
    
    * Bring uninstall of logging agent into script
    
    * Try and start opsagent at the end
    
    * Add catch for logging service
    
    * TRUE!

---
+++ scripts/OpsAgent/install_opsagent.ps1
@@ -0,0 +1,66 @@
+function Check_Service($Service_Name) {
+    if (Get-Service $Service_Name -ErrorAction SilentlyContinue) {
+        if (Get-Service $Service_Name | Where-Object {$_.Status -eq "Running"}) {
+            Write-Host "$Service_Name already started, nothing to do..."
+            return $TRUE
+        }
+        else {
+            Write-Host "$Service_Name found and running..."
+            return $TRUE
+        }
+    }
+    else {
+        Write-Host "Service $Service_Name not found, continuing..."
+        return $FALSE
+    }
+}
+
+function Uninstall-StackdriverLogging() {
+    Write-Host "Uninstalling Stackdriver Logging Agent"
+    $packageUninstall = Get-ItemPropertyValue 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\GoogleStackdriverLoggingAgent' -Name UninstallString
+    if ($packageUninstall) {
+      & $packageUninstall /S
+      Start-Sleep -s 5
+    }
+}
+
+if (Check_Service google-cloud-ops-agent) {
+    Write-Host "Google Cloud Ops agent running, executing update..."
+    googet -noconfirm remove google-cloud-ops-agent
+    googet -noconfirm install google-cloud-ops-agent
+}
+elseif (Check_Service StackdriverMonitoring) {
+    Write-Host "Old Stackdriver Monitoring agent is running, uninstalling and installing Ops Agent"
+    Stop-Service -Name StackdriverMonitoring
+    sc.exe delete StackdriverMonitoring
+    if (Check_Service StackdriverLogging) {
+        Uninstall-StackdriverLogging
+    }
+    (New-Object Net.WebClient).DownloadFile("https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.ps1", "$PSScriptRoot\add-google-cloud-ops-agent-repo.ps1")
+    $ErrorActionPreference = "Continue"
+    & "$PSScriptRoot\add-google-cloud-ops-agent-repo.ps1" -AlsoInstall
+}
+elseif (Check_Service StackdriverLogging) {
+    Write-Host "Old Stackdriver Logging agent is running, uninstalling and installing Ops Agent"
+    Uninstall-StackdriverLogging
+    (New-Object Net.WebClient).DownloadFile("https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.ps1", "$PSScriptRoot\add-google-cloud-ops-agent-repo.ps1")
+    $ErrorActionPreference = "Continue"
+    & "$PSScriptRoot\add-google-cloud-ops-agent-repo.ps1" -AlsoInstall
+}
+else {
+    Write-Host "No evidence of agents found, installing ops agent"
+    (New-Object Net.WebClient).DownloadFile("https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.ps1", "$PSScriptRoot\add-google-cloud-ops-agent-repo.ps1")
+    $ErrorActionPreference = "Continue"
+    & "$PSScriptRoot\add-google-cloud-ops-agent-repo.ps1" -AlsoInstall
+}
+
+Write-Host "Agent Installation Completed attempting to start it"
+try {
+    Start-Service google-cloud-ops-agent
+    Write-Host "Started Successfully"
+    exit 0
+}
+catch {
+    Write-Host "Failed to start, this is typically caused by the legacy agents still existing!"
+    exit 1
+}
