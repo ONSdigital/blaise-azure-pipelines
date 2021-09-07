3d487b796aa58e9adbcd26a12d70623b5894b941
elthorne <46898565+elthorne@users.noreply.github.com>
2021-09-07 14:22:20 +0100

    Installing Stackdriver logging

--- scripts/Stackdriver/install_stackdriver.ps1
+++ scripts/Stackdriver/install_stackdriver.ps1
@@ -1,32 +1,16 @@
 function Install_StackDriver_Logging() {
-    Write-Host "Checking if target logging agent version has been installed already..."
-    if (Test-Path C:\dev\data\$($LoggingAgent)) {
-        Write-Host "Version already installed, checking it has been started"
-        if (Get-Service "StackdriverLogging" | Where-Object {$_.Status -eq "Running"}) {
-            Write-Host "Already started, nothing to do..."
-        }
-        elseif (Get-Service "StackdriverLogging" | Where-Object {$_.Status -eq "Stopped"}) {
-            Write-Host "Starting service"
-            Start-Service -Name "StackdriverLogging"
-        }
-        else {
-            Write-Host "Error, service not found..."
-            exit 1
-        }
-    }
-    else {
-        Write-Host "Downloading Stackdriver logging agent installer from '$GCP_BUCKET'..."
-        gsutil cp gs://$GCP_BUCKET/$loggingagent "C:\dev\data\$($loggingagent)"
-
-        Write-Host "Installing Stackdriver logging agent..."
-        $logging_args = "/S /D='C:\dev\stackdriver\loggingAgent'"
-        Start-Process -Wait "C:\dev\data\$($loggingagent)" -ArgumentList $logging_args
-    }
+    curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+    sudo bash install-logging-agent.sh
 }
 
 function Uninstall_OpsAgent() {
     Write-Host "Uninstalling Ops Agent"
     googet -noconfirm remove google-cloud-ops-agent
+
+    Write-Host "Sanity checking Stackdriver Logging has been installed..."
+    if (Check_Service StackdriverMonitoring) {
+        Write-Host "Stackdriver Monitoring Agent is running"
+    }
 }
 
 function Check_Service($Service_Name) {
