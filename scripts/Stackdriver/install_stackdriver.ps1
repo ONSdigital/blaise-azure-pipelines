6c1eb098b5c4eedd75bbdfe7b7ba527dfdcfcb70
Elinor Thorne <46898565+elthorne@users.noreply.github.com>
2021-09-15 09:39:44 +0100

    Add stackdriver lu8865 (#44)
    * Re-implement Stackdriver
    
    * Check and uninstall Ops Agent
    
    * Correct whitespacing error
    
    * Hard code logging and monitoring agents for now
    
    * Close bracket
    
    * Refactor write-host messages
    
    * Install Stackdriver logging
    
    * Remove param
    
    * Installing Stackdriver logging
    
    * Installing Stackdriver logging
    
    * Installing Stackdriver logging
    
    * Installing Stackdriver logging
    
    * debugging
    
    * Trying different method of installing Stackdriver Logging agent
    
    * Debug
    
    * Fix whitespacing
    
    * Correct variable names
    
    * debugging
    
    * debugging
    
    * debugging
    
    * debugging
    
    * hard-coding variables for now
    
    * more debugging
    
    * correct spelling
    
    * more debugging
    
    * add params
    
    * additional condition for Stackdriver Logging
    
    * debug conditionals
    
    * undo conditionals
    
    * Uninstall Ops Agent if running and install Stackdriver logging and monitoring if they're not running
    
    * Uninstall Ops Agent, install Stackdriver agents, try/catch start Stackdriver services
    
    * Attempting to get service Stackdriver Monitoring
    
    * Confirming Stackdriver Logging agent works
    
    * Debug StackdriverMonitoring
    
    * Debug StackdriverMonitoring
    
    * Trying Stackdriver Monitoring once more with feeling
    
    * Trying a new approach to installing Stackdriver Monitoring
    
    * What happens when I only work with Stackdriver Monitoring and not Stackdriver Logging???
    
    * try, catch Stackdriver Monitoring
    
    * hackedy-hack
    
    * Check all the services
    
    * Remove some guff
    
    * one final attempt to debug
    
    * new approach by uninstalling everything ready to install fresh
    
    * fix uninstall
    
    * correct typo
    
    * deploy to nodes with blaise, data-delivery and restapi tags
    
    * like dis? lol
    
    * I think it's more like dis
    
    * does this work?
    
    * flipping whitespacing
    
    * how about this?
    
    * I'm not convinced data-delivery needs Stackdriver
    
    * why has the uninstall string changed since yesterday!?
    
    * debug
    
    * debug
    
    * staaahhhp
    
    * remove hard-coded variables
    
    * rollback
    
    * remove hard-coded variables

---
+++ scripts/Stackdriver/install_stackdriver.ps1
@@ -0,0 +1,78 @@
+param ([string]$loggingagent, [string]$monitoringagent, [string]$GCP_BUCKET)
+
+function Check_Service_Exists($Service_Name) {
+    if (Get-Service $Service_Name -ErrorAction SilentlyContinue) {
+        return $TRUE
+    }
+    return $FALSE
+}
+
+function Uninstall_Ops_Agent() {
+    Write-Host "Attempting to uninstall Ops Agent..."
+    googet -noconfirm remove google-cloud-ops-agent
+}
+
+function Uninstall_StackDriver_Logging() {
+    Write-Host "Uninstalling Stackdriver Logging Agent"
+
+    $currentPath = Get-Location
+    Write-Host "DEBUG: pwd: $currentPath"
+
+    Stop-Service -Name StackdriverLogging
+
+    $packageUninstall = "C:\Program Files (x86)\Stackdriver\LoggingAgent\uninstall.exe"
+    Write-Host "DEBUG: Uninstall string: '$packageUninstall'"
+    if ($packageUninstall) {
+      & $packageUninstall /S
+      Start-Sleep -s 5
+    }
+}
+
+function Uninstall_StackDriver_Monitoring() {
+    Write-Host "Uninstalling Stackdriver Monitoring Agent"
+
+    Stop-Service -Name StackdriverMonitoring
+
+    $packageUninstall = "C:\Program Files (x86)\Stackdriver\MonitoringAgent\uninstall.exe"
+    if ($packageUninstall) {
+      & $packageUninstall /S
+      Start-Sleep -s 5
+    }
+}
+
+function Install_StackDriver_Logging($loggingagent, $GCP_BUCKET) {
+    Write-Host "Downloading Stackdriver logging agent installer from '$GCP_BUCKET'..."
+    gsutil cp gs://$GCP_BUCKET/$loggingagent "C:\dev\data\$($loggingagent)"
+
+    Write-Host "Installing Stackdriver logging agent..."
+    $logging_args = "/S /D='C:\dev\stackdriver\loggingAgent'"
+    Start-Process -Wait "C:\dev\data\$($loggingagent)" -ArgumentList $logging_args
+}
+
+function Install_StackDriver_Monitoring($monitoringagent, $GCP_BUCKET) {
+    Write-Host "Downloading Stackdriver monitoring agent installer from '$($GCP_BUCKET)'..."
+    gsutil cp gs://$GCP_BUCKET/$monitoringagent "C:\dev\data\$($monitoringagent)"
+
+    Write-Host "Installing Stackdriver monitoring agent..."
+    $monitoring_args = "/S /D='C:\dev\stackdriver\monitoringAgent'"
+    Start-Process -Wait "C:\dev\data\$($monitoringagent)" -ArgumentList $monitoring_args
+}
+
+Write-Host "Target logging agent is: $($loggingagent)"
+Write-Host "Target monitoring agent is: $($monitoringagent)"
+Write-Host "GCP artifact bucket is: $($GCP_BUCKET)"
+
+if (Check_Service_Exists google-cloud-ops-agent) {
+    Uninstall_Ops_Agent
+}
+
+if (Check_Service_Exists StackdriverLogging) {
+    Uninstall_StackDriver_Logging
+}
+
+if (Check_Service_Exists StackdriverMonitoring) {
+    Uninstall_StackDriver_Monitoring
+}
+
+Install_StackDriver_Logging $loggingagent $GCP_BUCKET
+Install_StackDriver_Monitoring $monitoringagent $GCP_BUCKET
