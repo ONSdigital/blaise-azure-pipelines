e069c18706978fcd41f394404fb8da2e8a5d724e
Nik Webb <nik.webb@ons.gov.uk>
2021-09-27 14:36:22 +0100

    added checks to make sure blaise is registered

--- scripts/Blaise/update_blaise_license.ps1
+++ scripts/Blaise/update_blaise_license.ps1
@@ -1,29 +1,35 @@
 . "$PSScriptRoot\..\helpers\update_environment_variables.ps1"
 
 function SetBlaiseLicenseViaRegistry {
-    $licenseInfo = Get-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey'
-    $activationInfo = Get-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode'
+    if (Test-Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0') {
+        $licenseInfo = Get-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey'
+        $activationInfo = Get-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode'
 
-    if ($licenseInfo.LicenseKey -eq $BLAISE_SERIALNUMBER)
-    {
-        Write-Host "Serial number is correct: $($BLAISE_SERIALNUMBER)"
-    }
-    else
-    {
-        Write-Host "Serial number is out of date"
-        Set-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey' -value $BLAISE_SERIALNUMBER
-        Write-Host "Serial number updated to: $($BLAISE_SERIALNUMBER)"
-    }
+        if ($licenseInfo.LicenseKey -eq $BLAISE_SERIALNUMBER)
+        {
+            Write-Host "Serial number is correct: $($BLAISE_SERIALNUMBER)"
+        }
+        else
+        {
+            Write-Host "Serial number is out of date"
+            Set-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey' -value $BLAISE_SERIALNUMBER
+            Write-Host "Serial number updated to: $($BLAISE_SERIALNUMBER)"
+        }
 
-    if ($activationInfo.ActivationCode -eq $BLAISE_ACTIVATIONCODE)
-    {
-        Write-Host "Activation code is correct: $($BLAISE_ACTIVATIONCODE)"
+        if ($activationInfo.ActivationCode -eq $BLAISE_ACTIVATIONCODE)
+        {
+            Write-Host "Activation code is correct: $($BLAISE_ACTIVATIONCODE)"
+        }
+        else
+        {
+            Write-Host "Activation code is out of date"
+            Set-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode' -value $BLAISE_ACTIVATIONCODE
+            Write-Host "Activation code updated to: $($BLAISE_ACTIVATIONCODE)"
+        }
     }
-    else
+    else 
     {
-        Write-Host "Activation code is out of date"
-        Set-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode' -value $BLAISE_ACTIVATIONCODE
-        Write-Host "Activation code updated to: $($BLAISE_ACTIVATIONCODE)"
+        Write-Host "No registry key found for Blaise"
     }
 }
 
