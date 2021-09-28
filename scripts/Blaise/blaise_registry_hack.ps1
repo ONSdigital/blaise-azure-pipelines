037e1a57b2c575d7a78432c6c2e471f58e39ffc1
Nik Webb <nik.webb@ons.gov.uk>
2021-09-28 16:47:37 +0100

    removed parameters

--- scripts/Blaise/blaise_registry_hack.ps1
+++ scripts/Blaise/blaise_registry_hack.ps1
@@ -1,5 +1,3 @@
-param ([string] $BlaiseLicenseKey, [string] $BlaiseActivationCode)
-
 . "$PSScriptRoot\..\helpers\update_environment_variables.ps1"
 . "$PSScriptRoot\update_blaise_license.ps1"
 
@@ -9,8 +7,8 @@ if (Test-Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0') {
 }
 else {
     New-Item -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Force
-    New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey' -value $BlaiseLicenseKey
-    New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode' -value $BlaiseActivationCode
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey' -value $env:BlaiseLicenseKey
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode' -value $env:BlaiseActivationCode
     New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'Licensee' -value "ONS"
     New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'CompanyName' -value "ONS"
     New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'UserName' -value 'ONS-USER'
--- scripts/Blaise/update_blaise_license.ps1
+++ scripts/Blaise/update_blaise_license.ps1
@@ -1,37 +1,29 @@
-param (
-    $BlaiseLicenseKey,
-    $BlaiseActivationCode
-    )
-    
+   
 function SetBlaiseLicenseViaRegistry {
-    param (
-    [string] $Blaise_License_Key = $BlaiseLicenseKey,
-    [string] $Blaise_Activation_Code = $BlaiseActivationCode
-    )
     if (Test-Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0') {
         $licenseInfo = Get-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey'
         $activationInfo = Get-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode'
 
-        if ($licenseInfo.LicenseKey -eq $Blaise_License_Key)
+        if ($licenseInfo.LicenseKey -eq $env:Blaise_License_Key)
         {
-            Write-Host "Serial number is correct: $($Blaise_License_Key)"
+            Write-Host "Serial number is correct: $($env:Blaise_License_Key)"
         }
         else
         {
             Write-Host "Serial number is out of date"
-            Set-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey' -value $Blaise_License_Key
-            Write-Host "Serial number updated to: $($Blaise_License_Key)"
+            Set-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey' -value $env:Blaise_License_Key
+            Write-Host "Serial number updated to: $($env:Blaise_License_Key)"
         }
 
-        if ($activationInfo.ActivationCode -eq $Blaise_Activation_Code)
+        if ($activationInfo.ActivationCode -eq $env:Blaise_Activation_Code)
         {
-            Write-Host "Activation code is correct: $($Blaise_Activation_Code)"
+            Write-Host "Activation code is correct: $($env:Blaise_Activation_Code)"
         }
         else
         {
             Write-Host "Activation code is out of date"
-            Set-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode' -value $Blaise_Activation_Code
-            Write-Host "Activation code updated to: $($Blaise_Activation_Code)"
+            Set-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode' -value $env:Blaise_Activation_Code
+            Write-Host "Activation code updated to: $($env:Blaise_Activation_Code)"
         }
     }
     else 
