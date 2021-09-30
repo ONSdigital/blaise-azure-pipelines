78ba5a19ab60d37e289366b7d1a8d6fa04d85db9
Nik Webb <nik.webb@ons.gov.uk>
2021-09-30 14:07:20 +0100

    this should work

--- scripts/Blaise/blaise_registry_hack.ps1
+++ scripts/Blaise/blaise_registry_hack.ps1
@@ -1,3 +1,4 @@
+param ([string]$BLAISE_LICENSE_KEY, [string]$BLAISE_ACTIVATION_CODE)
 . "$PSScriptRoot\..\helpers\update_environment_variables.ps1"
 . "$PSScriptRoot\update_blaise_license.ps1"
 
@@ -7,8 +8,8 @@ if (Test-Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0') {
 }
 else {
     New-Item -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Force
-    New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey' -value $(ENV_BLAISE_LICENSE_KEY)
-    New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode' -value $(ENV_BLAISE_ACTIVATION_CODE)
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'LicenseKey' -value $($BLAISE_LICENSE_KEY)
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'ActivationCode' -value $($BLAISE_ACTIVATION_CODE)
     New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'Licensee' -value "ONS"
     New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'CompanyName' -value "ONS"
     New-ItemProperty -Path 'HKLM:\SOFTWARE\StatNeth\Blaise\5.0' -Name 'UserName' -value 'ONS-USER'
