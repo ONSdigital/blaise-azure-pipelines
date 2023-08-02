ca4c5693b19d049feb7a4d13a9ece562df58f58c
elthorne <46898565+elthorne@users.noreply.github.com>
2023-08-02 11:51:55 +0100

    wip: halfway there
    refs: BLAIS5-3855

--- scripts/Blaise/mysql_database_setup.ps1
+++ scripts/Blaise/mysql_database_setup.ps1
@@ -1,20 +1,5 @@
 . "$PSScriptRoot\..\helpers\data_interface_files.ps1"
 
-function ConfigurationChangesDetected {
-    param (
-        [string] $DatabaseFilePath
-    )
-    $configurationSettings = ListOfConfigurationSettings
-
-    if ($configurationSettings.contains($DatabaseFilePath)) {
-        Write-Host "No changes detected in $DatabaseFilePath. Blaise restart not required."   
-        return $false
-    
-    }
-    Write-Host "Changes detected in $DatabaseFilePath. Blaise restart is required."
-    return $true
-}
-
 function XMLConfigurationChangesDetected {
     param (
         [string] $DatabaseFilePath,
@@ -29,23 +14,22 @@ function XMLConfigurationChangesDetected {
     Write-Host "Changes detected in $DatabaseFilePath. Blaise restart is required."
     return $true
 }
-    $restartBlaise = ,$false
+    $originalConfiguration = ListOfConfigurationSettings
+    $originalXmlConfiguration = [xml](Get-Content "C:\Blaise5\Bin\StatNeth.Blaise.Runtime.ServicesHost.exe.config")
+    Write-Host "DEBUG: originalXmlConfiguration: $originalXmlConfiguration"
 
     #audit
     $audit_db_file_path = "D:\Blaise5\Settings\audittraildb.badi"
-    $restartBlaise += ConfigurationChangesDetected -DatabaseFilePath $audit_db_file_path
     CreateDataInterfaceFile -filePath $audit_db_file_path -applicationType audittrail
     RegisterDataInterfaceFile -filePath $audit_db_file_path -registerCommand audittraildatainterface
 
     #Session
     $session_db_file_path = "D:\Blaise5\Settings\sessiondb.bsdi"
-    $restartBlaise += ConfigurationChangesDetected -DatabaseFilePath $session_db_file_path
     CreateDataInterfaceFile -filePath $session_db_file_path -applicationType session
     RegisterDataInterfaceFile -filePath $session_db_file_path -registerCommand sessiondatainterface
 
     #Cati
     $cati_db_file_path = "D:\Blaise5\Settings\catidb.bcdi"
-    $restartBlaise += ConfigurationChangesDetected -DatabaseFilePath $cati_db_file_path
     CreateDataInterfaceFile -filePath $cati_db_file_path -applicationType cati
     RegisterDataInterfaceFile -filePath $cati_db_file_path -registerCommand catidatainterface
 
@@ -56,10 +40,12 @@ function XMLConfigurationChangesDetected {
     CreateDataInterfaceFile -filePath $config_db_file_path -applicationType configuration
     RegisterDatainterfaceViaXML -filePath $config_db_file_path -configFile $config_file_path -interfaceFileName "ConfigurationDataInterfaceFile"
     
-    if ($restartBlaise.Contains($true)) {
+    #Restart Blaise if required
+    $newConfiguration = ListOfConfigurationSettings
+    if ($originalConfiguration -ne $newConfiguration) {
         Write-Host "Changes have been detected. Restarting Blaise..."
         restart-service blaiseservices5
     } else {
-        Write-Host "DEBUG: Blaise was not restarted :tada:"
+        Write-Host "DEBUG 3: Blaise was not restarted :tada:"
     }
 
