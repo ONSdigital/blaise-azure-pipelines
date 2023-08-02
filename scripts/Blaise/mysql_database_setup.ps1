e50dc51bf3ee5ba69466f50bea9ea14d06dd8d2d
elthorne <46898565+elthorne@users.noreply.github.com>
2023-08-02 12:02:31 +0100

    wip: 3/4s the way there!
    refs: BLAIS5-3855

--- scripts/Blaise/mysql_database_setup.ps1
+++ scripts/Blaise/mysql_database_setup.ps1
@@ -1,22 +1,24 @@
 . "$PSScriptRoot\..\helpers\data_interface_files.ps1"
 
-function XMLConfigurationChangesDetected {
-    param (
-        [string] $DatabaseFilePath,
-        [string] $ConfigFilePath
-    )
-    $xml = [xml](Get-Content $ConfigFilePath)
+# function XMLConfigurationChangesDetected {
+#     param (
+#         [string] $DatabaseFilePath,
+#         [string] $ConfigFilePath
+#     )
+#     $xml = [xml](Get-Content $ConfigFilePath)
 
-    if($xml.InnerXml.Contains($DatabaseFilePath)){
-        Write-Host "No changes detected in $DatabaseFilePath. Blaise restart not required."   
-        return $false
-    }
-    Write-Host "Changes detected in $DatabaseFilePath. Blaise restart is required."
-    return $true
-}
+#     if($xml.InnerXml.Contains($DatabaseFilePath)){
+#         Write-Host "No changes detected in $DatabaseFilePath. Blaise restart not required."   
+#         return $false
+#     }
+#     Write-Host "Changes detected in $DatabaseFilePath. Blaise restart is required."
+#     return $true
+# }
     $originalConfiguration = ListOfConfigurationSettings
     $originalXmlConfiguration = [xml](Get-Content "C:\Blaise5\Bin\StatNeth.Blaise.Runtime.ServicesHost.exe.config")
-    Write-Host "DEBUG: originalXmlConfiguration: $originalXmlConfiguration"
+    
+    $printDis = $originalXmlConfiguration.InnerXml
+    Write-Host "DEBUG: originalXmlConfiguration: $printDis"
 
     #audit
     $audit_db_file_path = "D:\Blaise5\Settings\audittraildb.badi"
@@ -36,16 +38,23 @@ function XMLConfigurationChangesDetected {
     #Config
     $config_db_file_path = "D:\Blaise5\Settings\configurationdb.bidi"
     $config_file_path = "C:\Blaise5\Bin\StatNeth.Blaise.Runtime.ServicesHost.exe.config"
-    $restartBlaise += XMLConfigurationChangesDetected -DatabaseFilePath $config_db_file_path -ConfigFilePath $config_file_path
     CreateDataInterfaceFile -filePath $config_db_file_path -applicationType configuration
     RegisterDatainterfaceViaXML -filePath $config_db_file_path -configFile $config_file_path -interfaceFileName "ConfigurationDataInterfaceFile"
     
     #Restart Blaise if required
     $newConfiguration = ListOfConfigurationSettings
+    $newXmlConfiguration = [xml](Get-Content "C:\Blaise5\Bin\StatNeth.Blaise.Runtime.ServicesHost.exe.config")
     if ($originalConfiguration -ne $newConfiguration) {
         Write-Host "Changes have been detected. Restarting Blaise..."
         restart-service blaiseservices5
     } else {
         Write-Host "DEBUG 3: Blaise was not restarted :tada:"
+    }
+
+    if($originalXmlConfiguration.InnerXml -ne $newXmlConfiguration.InnerXml) {
+        Write-Host "Changes have been detected. Restarting Blaise..."
+        restart-service blaiseservices5
+    } else {
+        Write-Host "DEBUG 4: Blaise was not restarted :tada:"
     }
 
