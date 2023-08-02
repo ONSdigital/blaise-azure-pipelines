5428d4d5c0e10cb3bcfc51a67483259ae343b32a
elthorne <46898565+elthorne@users.noreply.github.com>
2023-08-02 11:34:45 +0100

    refactor: Will this work? Is it simpler?
    refs: BLAIS5-3855

--- scripts/Blaise/mysql_database_setup.ps1
+++ scripts/Blaise/mysql_database_setup.ps1
@@ -29,35 +29,41 @@ function XMLConfigurationChangesDetected {
     Write-Host "Changes detected in $DatabaseFilePath. Blaise restart is required."
     return $true
 }
-    $restartBlaise = ,$false
+
+    $originalConfigurationSettings = ListOfConfigurationSettings
 
     #audit
     $audit_db_file_path = "D:\Blaise5\Settings\audittraildb.badi"
-    $restartBlaise += ConfigurationChangesDetected -DatabaseFilePath $audit_db_file_path
     CreateAndRegisterDataInterfaceFile -DatabaseFilePath $audit_db_file_path -ApplicationType audittrail -RegisterCommand audittraildatainterface
 
     #Session
     $session_db_file_path = "D:\Blaise5\Settings\sessiondb.bsdi"
-    $restartBlaise += ConfigurationChangesDetected -DatabaseFilePath $session_db_file_path
     CreateAndRegisterDataInterfaceFile -DatabaseFilePath $session_db_file_path -ApplicationType session -RegisterCommand sessiondatainterface
 
     #Cati
     $cati_db_file_path = "D:\Blaise5\Settings\catidb.bcdi"
-    $restartBlaise += ConfigurationChangesDetected -DatabaseFilePath $cati_db_file_path
     CreateAndRegisterDataInterfaceFile -DatabaseFilePath $cati_db_file_path -ApplicationType cati -RegisterCommand catidatainterface
     
     #Config
     $config_db_file_path = "D:\Blaise5\Settings\configurationdb.bidi"
     $config_file_path = "C:\Blaise5\Bin\StatNeth.Blaise.Runtime.ServicesHost.exe.config"
-    $restartBlaise += XMLConfigurationChangesDetected -DatabaseFilePath $config_db_file_path -ConfigFilePath $config_file_path
 
     CreateDataInterfaceFile -filePath $config_db_file_path -applicationType configuration
     RegisterDatainterfaceViaXML -filePath $config_db_file_path -configFile $config_file_path -interfaceFileName "ConfigurationDataInterfaceFile"
     
-    # Restart Blaise if changes detected
-    if ($restartBlaise.Contains($true)) {
+    # # Restart Blaise if changes detected
+    # if ($restartBlaise.Contains($true)) {
+    #     Write-Host "Changes have been detected. Restarting Blaise..."
+    #     restart-service blaiseservices5
+    # } else {
+    #     Write-Host "DEBUG: Blaise was not restarted :tada:"
+    # }
+
+    $NewConfigurationSettings = ListOfConfigurationSettings
+    if ($originalConfigurationSettings -ne $newConfigurationSettings) {
         Write-Host "Changes have been detected. Restarting Blaise..."
         restart-service blaiseservices5
     } else {
-        Write-Host "DEBUG: Blaise was not restarted :tada:"
+        Write-Host "DEBUG 2: Blaise was not restarted :tada:"
     }
+
