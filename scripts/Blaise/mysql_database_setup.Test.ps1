89f99a2488c683cf0d1e6f819b127ef8368b9e7a
elthorne <46898565+elthorne@users.noreply.github.com>
2023-08-02 08:59:33 +0100

    test: My first Pester unit test :tada:
    I have no idea if it's going to work? I haven't run it yet!
    
    refs: BLAIS5-3855

---
+++ scripts/Blaise/mysql_database_setup.Test.ps1
@@ -0,0 +1,28 @@
+BeforeAll {
+    . "$PSScriptRoot\mysql_database_setup.ps1",
+    . "$PSScriptRoot\..\helpers\data_interface_files.ps1"
+}
+
+Describe 'RestartBlaiseRequired' {
+
+    It 'returns true when changes were detected in the configuration settings for the Session Data Interface' {
+        # Arrange
+        $configurationSettings =  @"
+-----------------------------------------------------
+|              Configuration settings               |
+-----------------------------------------------------
+| Setting                 | Value            | Used |
+-----------------------------------------------------
+| Session Data Interface  |                  | Yes  |
+-----------------------------------------------------
+"@ 
+        Mock ListOfConfigurationSettings { return $configurationSettings }
+        $filePath = "D:\Blaise5\Settings\sessiondb.bsdi"
+
+        # Act
+        $result = RestartBlaiseRequired -filePath $filePath
+
+        # Assert
+        $result | Should -Be $true
+    }
+}
