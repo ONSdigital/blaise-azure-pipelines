55599e97ebd9a57dce978167a96db293ff760fcc
elthorne <46898565+elthorne@users.noreply.github.com>
2023-08-02 15:55:25 +0100

    refactor: Debug statements removed
    refs: BLAIS5-3855

--- scripts/Blaise/mysql_database_setup.ps1
+++ scripts/Blaise/mysql_database_setup.ps1
@@ -1,8 +1,10 @@
 . "$PSScriptRoot\..\helpers\data_interface_files.ps1"
 
     $originalConfiguration = ListOfConfigurationSettings
-    Write-Host "DEBUG: originalConfiguration: $originalConfiguration"
     $originalXmlConfiguration = [xml](Get-Content "C:\Blaise5\Bin\StatNeth.Blaise.Runtime.ServicesHost.exe.config")
+
+    # TRIGGER A RESTART!!!
+    # $originalConfiguration = $originalConfiguration.Replace('D:\Blaise5\Settings\audittraildb.badi','TRIGGERED!')
     
     #audit
     $audit_db_file_path = "D:\Blaise5\Settings\audittraildb.badi"
@@ -26,25 +28,16 @@
 
     #Credentials
     #Commenting out calling MySQL data interface creation for creds until upgrade to Blaise 5.13
-    $credentials_db_file_path = "D:\Blaise5\Settings\credentials.budi"
-    CreateDataInterfaceFile -filePath $credentials_db_file_path -applicationType credentials
-    RegisterDataInterfaceFile -filePath $credentials_db_file_path -registerCommand credentialsdatainterface
+    # $credentials_db_file_path = "D:\Blaise5\Settings\credentials.budi"
+    # CreateDataInterfaceFile -filePath $credentials_db_file_path -applicationType credentials
+    # RegisterDataInterfaceFile -filePath $credentials_db_file_path -registerCommand credentialsdatainterface
 
     #Restart Blaise if required
     $newConfiguration = ListOfConfigurationSettings
-    Write-Host "DEBUG: newConfiguration: $newConfiguration"
     $newXmlConfiguration = [xml](Get-Content "C:\Blaise5\Bin\StatNeth.Blaise.Runtime.ServicesHost.exe.config")
-
-    # TRIGGER A RESTART!!!
-    $elsTrigger = $originalConfiguration.Replace('D:\Blaise5\Settings\audittraildb.badi','TRIGGERED!')
-    Write-Host "DEBUG: elsTrigger: $elsTrigger"
     
-    # $configurationChangesDetected = $originalConfiguration -ne $newConfiguration
-    $configurationChangesDetected = $elsTrigger -ne $newConfiguration
-    Write-Host "DEBUG: Changes detected in config: $configurationChangesDetected"
-
+    $configurationChangesDetected = $originalConfiguration -ne $newConfiguration
     $xmlConfigurationChangesDetected = $originalXmlConfiguration.InnerXml -ne $newXmlConfiguration.InnerXml
-    Write-Host "DEBUG: Changes detected in xml config: $xmlConfigurationChangesDetected"
 
     if ($configurationChangesDetected -or $xmlConfigurationChangesDetected) {
         Write-Host "Changes have been detected. Restarting Blaise..."
