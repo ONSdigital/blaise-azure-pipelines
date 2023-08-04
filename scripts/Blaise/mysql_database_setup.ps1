a66bcc2e2fc5b900e6058b1b03f0931f0de12743
social-surveys-blaise-concourse <79969802+social-surveys-blaise-concourse@users.noreply.github.com>
2023-08-04 13:34:30 +0100

    Promote from BLAIS5-3855 to main (#98)
    * feat: Only restart Blaise when changes have been detected
    
    refs: BLAIS5-3855
    Co-authored-by: elthorne <46898565+elthorne@users.noreply.github.com>

--- scripts/Blaise/mysql_database_setup.ps1
+++ scripts/Blaise/mysql_database_setup.ps1
@@ -1,29 +1,35 @@
 . "$PSScriptRoot\..\helpers\data_interface_files.ps1"
 
+    $newDataInterfaceCreated = ,$false
+    $newDataInterfaceRegistered = ,$false
+    
     #audit
     $audit_db_file_path = "D:\Blaise5\Settings\audittraildb.badi"
-    CreateDataInterfaceFile -filePath $audit_db_file_path -applicationType audittrail
-    RegisterDataInterfaceFile -filePath $audit_db_file_path -registerCommand audittraildatainterface
+    $newDataInterfaceCreated += CreateDataInterfaceFile -filePath $audit_db_file_path -applicationType audittrail
+    $newDataInterfaceRegistered += RegisterDataInterfaceFile -filePath $audit_db_file_path -registerCommand audittraildatainterface
 
     #Session
     $session_db_file_path = "D:\Blaise5\Settings\sessiondb.bsdi"
-    CreateDataInterfaceFile -filePath $session_db_file_path -applicationType session
-    RegisterDataInterfaceFile -filePath $session_db_file_path -registerCommand sessiondatainterface
+    $newDataInterfaceCreated += CreateDataInterfaceFile -filePath $session_db_file_path -applicationType session
+    $newDataInterfaceRegistered += RegisterDataInterfaceFile -filePath $session_db_file_path -registerCommand sessiondatainterface
 
     #Cati
     $cati_db_file_path = "D:\Blaise5\Settings\catidb.bcdi"
-    CreateDataInterfaceFile -filePath $cati_db_file_path -applicationType cati
-    RegisterDataInterfaceFile -filePath $cati_db_file_path -registerCommand catidatainterface
+    $newDataInterfaceCreated += CreateDataInterfaceFile -filePath $cati_db_file_path -applicationType cati
+    $newDataInterfaceRegistered += RegisterDataInterfaceFile -filePath $cati_db_file_path -registerCommand catidatainterface
 
     #Config
     $config_db_file_path = "D:\Blaise5\Settings\configurationdb.bidi"
-    CreateDataInterfaceFile -filePath $config_db_file_path -applicationType configuration
-    RegisterDatainterfaceViaXML -filePath $config_db_file_path -configFile "C:\Blaise5\Bin\StatNeth.Blaise.Runtime.ServicesHost.exe.config" -interfaceFileName "ConfigurationDataInterfaceFile"
+    $newDataInterfaceCreated += CreateDataInterfaceFile -filePath $config_db_file_path -applicationType configuration
+    $newDataInterfaceRegistered += RegisterDatainterfaceViaXML -filePath $config_db_file_path -configFile "C:\Blaise5\Bin\StatNeth.Blaise.Runtime.ServicesHost.exe.config" -interfaceFileName "ConfigurationDataInterfaceFile"
 
     #Credentials
     #Commenting out calling MySQL data interface creation for creds until upgrade to Blaise 5.13
-    #$credentials_db_file_path = "D:\Blaise5\Settings\credentials.budi"
-    #CreateDataInterfaceFile -filePath $credentials_db_file_path -applicationType credentials
-    #RegisterDataInterfaceFile -filePath $credentials_db_file_path -registerCommand credentialsdatainterface
+    # $credentials_db_file_path = "D:\Blaise5\Settings\credentials.budi"
+    # $newDataInterfaceCreated += CreateDataInterfaceFile -filePath $credentials_db_file_path -applicationType credentials
+    # $newDataInterfaceRegistered += RegisterDataInterfaceFile -filePath $credentials_db_file_path -registerCommand credentialsdatainterface
 
-    restart-service blaiseservices5
+    if ($NewDataInterfaceCreated.Contains($true) -or $NewDataInterfaceRegistered.Contains($true)){
+        Write-Host "Changes have been detected. Restarting Blaise..."
+        restart-service blaiseservices5
+    }
