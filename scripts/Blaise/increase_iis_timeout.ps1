29396e5a34d7b3bbce2ab48bd73418c6b4a34070
cal-nic <callum.nicholson@ons.gov.uk>
2023-08-07 15:23:25 +0100

    refactor: split out into separate files

--- scripts/Blaise/increase_iis_timeout.ps1
+++ scripts/Blaise/increase_iis_timeout.ps1
@@ -1,77 +1,8 @@
-function currentTimeoutValues {
-    $currentSessionStateTimeout = (Get-WebConfigurationProperty -filter system.web/sessionState -name Timeout -PSPath "IIS:\Sites\Default Web Site\Blaise").Value
-    Write-Host "Session timeout is currently: $currentSessionStateTimeout"
-    $currentIdleTimeout = (Get-ItemProperty ("IIS:\AppPools\BlaiseAppPool")).processModel.idleTimeout
-    Write-Host "Idle timeout is currently: $currentIdleTimeout"
-    return $currentSessionStateTimeout, $currentIdleTimeout
-}
-function timeoutIsSetCorrectly {
-    param (
-        [string] $currentTimeout,
-        [string] $expectedTimeout
-    )
-    Write-Host "current: $currentTimeout"
-    Write-Host "expected: $expectedTimeout"
-    return $currentTimeout -eq $expectedTimeout
-}
-
-
-[bool] $restartNeeded = $false
-[string] $expectedTimeout = "09:00:00"
-Write-Host "Getting current timeouts"
-$currentSessionStateTimeout, $currentIdleTimeout = currentTimeoutValues
-
-try{    
-    Write-Host "Checking session state time-out"
-    $setTimeout = timeoutIsSetCorrectly -currentTimeout $currentSessionStateTimeout -expectedTimeout $expectedTimeout
-    
-    if (-Not $setTimeout){
-        Set-WebConfigurationProperty system.web/sessionState "IIS:\Sites\Default Web Site\Blaise" -Name "Timeout" -Value:$expectedTimeout
-        $restartNeeded = $true
-        Write-Host "Session time-out set, Restart required"
-    }
-    else {
-        Write-Host "Timeout already set, Restart not required"
-    }    
-}
-catch{
-    Write-Host "Could not set session state time-out"
-    Write-Host $_.ScriptStackTrace
-    exit 1
-}
-
+. "$PSScriptRoot\check_iis_timeouts.ps1"
 try{
-    Write-Host "Checking ISS idle time-out"
-    $setTimeout = timeoutIsSetCorrectly -currentTimeout $currentSessionStateTimeout -expectedTimeout $expectedTimeout
-    if (-Not $setTimeout){
-        Set-ItemProperty ("IIS:\AppPools\BlaiseAppPool") -Name processModel.idleTimeout -value $expectedTimeout
-        $restartNeeded = $true
-        Write-Host "IIS idle time-out set, Restart required"
-    }
-    else {
-        Write-Host "Timeout already set, Restart not required"
-    }   
+    setTimeoutValues
 }
-catch{
-    Write-Host "Could not set ISS idle time-out"
-    Write-Host $_.ScriptStackTrace
-    exit 1
-}
-
-currentTimeoutValues
-
-try
-{
-    if ($restartNeeded){
-        Write-Host "Restarting BlaiseAppPool"
-        Restart-WebAppPool BlaiseAppPool
-        Write-Host "BlaiseAppPool has been restarted"
-    }
-    else{
-        Write-Host "BlaiseAppPool does not need to be restarted"
-    }    
-}
-catch{
-    Write-Host "Unable to restart BlaiseAppPool"
+catch {
+    Write-Host "Error checking/updating IIS timeouts. $($_.Exception.Message) at: $($_.ScriptStackTrace)"
     exit 1
 }
