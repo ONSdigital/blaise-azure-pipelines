93db2b7cdadcc176af82ae83238f4480b1ec4a78
cal-nic <callum.nicholson@ons.gov.uk>
2023-08-07 16:21:19 +0100

    chore: rename the test file

---
+++ scripts/Blaise/check_iis_timeout.Tests.ps1
@@ -0,0 +1,45 @@
+BeforeAll {. "$PSScriptRoot\check_iis_timeouts.ps1"}
+
+describe "timeoutIsSetCorrectly" {
+    BeforeEach{
+        Mock currentTimeoutValues{ return "00:00:00", "00:00:00" }
+        $currentSessionStateTimeout, $currentIdleTimeout = currentTimeoutValues
+    }
+    
+    it "should return true if the current timeouts are equal to the expected timeout" {
+        currentTimeoutValues
+        $expectedTimeout = "00:00:00"
+        $currentTimeoutIsSetCorrectly = timeoutIsSetCorrectly -currentSessionTimeout $currentSessionStateTimeout -currentIdleTimeout $currentIdleTimeout -expectedTimeout $expectedTimeout
+        $currentTimeoutIsSetCorrectly | Should -Be $true
+    }
+    it "should return false if the current timeouts are not equal to the expected timeout" {
+        currentTimeoutValues
+        $expectedTimeout = "00:00:01"
+        $currentTimeoutIsSetCorrectly = timeoutIsSetCorrectly -currentSessionTimeout $currentSessionStateTimeout -currentIdleTimeout $currentIdleTimeout -expectedTimeout $expectedTimeout
+        $currentTimeoutIsSetCorrectly | Should -Be $false
+    }
+}
+describe "setTimeoutValues" {
+    BeforeEach{
+        Mock Restart-WebAppPool {
+            # This mock function will be called instead of the actual Restart-WebAppPool
+        }
+    }
+
+    It "Restarts the app pool when timeouts have been set/changed" {
+        $currentSessionStateTimeout = "00:15:00"
+        $currentIdleTimeout = "09:00:00"
+        Mock currentTimeoutValues { return $currentSessionStateTimeout, $currentIdleTimeout}
+        setTimeoutValues 
+        Assert-MockCalled -CommandName Restart-WebAppPool -Exactly 1
+    }
+
+    It "Doesn't restart the app pool when timeouts are already set correctly" {
+        $currentSessionStateTimeout = "08:00:00"
+        $currentIdleTimeout = "08:00:00"
+        Mock currentTimeoutValues { return $currentSessionStateTimeout, $currentIdleTimeout}
+        setTimeoutValues 
+        Assert-MockCalled -CommandName Restart-WebAppPool -Exactly 0
+    }
+}
+
