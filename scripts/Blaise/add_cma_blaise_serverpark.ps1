95b9e1490580bd4110f030caa7bd45d1616741b6
jamie kerr <jamiestephenkerr@gmail.com>
2023-12-07 11:38:04 +0000

    Added additional serverparks

--- scripts/Blaise/add_cma_blaise_serverpark.ps1
+++ scripts/Blaise/add_cma_blaise_serverpark.ps1
@@ -1,11 +1,15 @@
 function ConfigureCmaServerpark{
     param(
-        [string] $ManagementNode = $env:ENV_BLAISE_SERVER_HOST_NAME,
-        [string] $ConnectionPort = $env:ENV_BLAISE_CONNECTION_PORT,
-        [string] $BlaisePassword = $env:ENV_BLAISE_ADMIN_PASSWORD,
-        [string] $BlaiseCmaServerPark = $env:ENV_BLAISE_CMA_SERVER_PARK_NAME,
-        [string] $BlaiseUserName = $env:ENV_BLAISE_ADMIN_USER
+        [string] $ServerParkName,
+        [string] $ManagementNode,
+        [string] $ConnectionPort,
+        [string] $BlaiseUserName,
+        [string] $BlaisePassword,
     )
+
+    If ([string]::IsNullOrEmpty($ServerParkName)) {
+        throw [System.IO.ArgumentException] "No server park name argument provided"
+    }
 
     If ([string]::IsNullOrEmpty($ManagementNode)) {
         throw [System.IO.ArgumentException] "No management node argument provided"
@@ -15,30 +19,34 @@ function ConfigureCmaServerpark{
         throw [System.IO.ArgumentException] "No Blaise connection port argument provided"
     }
 
-    If ([string]::IsNullOrEmpty($BlaisePassword)) {
-        throw [System.IO.ArgumentException] "No Blaise Admin Password argument provided"
-    }
-
-    If ([string]::IsNullOrEmpty($BlaiseCmaServerPark)) {
-        throw [System.IO.ArgumentException] "No Blaise CMA server park argument provided"
-    }
-
     If ([string]::IsNullOrEmpty($BlaiseUserName)) {
         throw [System.IO.ArgumentException] "No Blaise username argument provided"
     }
 
-    Write-Host "Add and configure CMA server park to run in disconnected mode"
+    If ([string]::IsNullOrEmpty($BlaisePassword)) {
+        throw [System.IO.ArgumentException] "No Blaise Admin Password argument provided"
+    }
+
+    Write-Host "Add and/or configure server park '$ServerParkName'"
 
     #if the serverpark exists this will update the existing one
-    c:\blaise5\bin\servermanager -addserverpark:$BlaiseCmaServerPark -runmode:disconnected -server:$managementNode -syncsurveyswhenconnected:true -binding:http -port:$connectionPort -user:$blaiseUserName -password:$blaisePassword
+    c:\blaise5\bin\servermanager -addserverpark:$ServerParkName -runmode:disconnected -server:$managementNode -syncsurveyswhenconnected:true -binding:http -port:$connectionPort -user:$blaiseUserName -password:$blaisePassword
 
-    Write-Host "Configured CMA server park to run in disconnected mode"
+    Write-Host "Configured server park '$ServerParkName'"
 }
 
 try{
-    ConfigureCmaServerpark
+    $BlaiseCmaServerPark = $env:ENV_BLAISE_CMA_SERVER_PARK_NAME,
+    $ManagementNode = $env:ENV_BLAISE_SERVER_HOST_NAME,
+    $ConnectionPort = $env:ENV_BLAISE_CONNECTION_PORT,
+    $BlaisePassword = $env:ENV_BLAISE_ADMIN_PASSWORD,
+    $BlaiseUserName = $env:ENV_BLAISE_ADMIN_USER
+
+    ConfigureCmaServerpark($BlaiseCmaServerPark, $ManagementNode, $ConnectionPort, $BlaiseUserName, $BlaisePassword)
+    ConfigureCmaServerpark($BlaiseCmaServerPark + "_APP", $ManagementNode, $ConnectionPort, $BlaiseUserName, $BlaisePassword)
+    ConfigureCmaServerpark($BlaiseCmaServerPark + "_ADMIN", $ManagementNode, $ConnectionPort, $BlaiseUserName, $BlaisePassword)
 }
 catch{
-    Write-Host "Adding and/or configuring CMA server park to run in disconnected mode failed: $($_.ScriptStackTrace)"
+    Write-Host "Adding and/or configuring CMA server parks failed: $($_.ScriptStackTrace)"
     exit 1
 }
