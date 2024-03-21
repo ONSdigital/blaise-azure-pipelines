dc8ece1577b3a0a8ed9ef5dcd2328f9e07a98278
riceyo <richmond@ricey.org>
2024-03-21 22:55:10 +0000

    refactor

--- scripts/Blaise/install_cma_packages.ps1
+++ scripts/Blaise/install_cma_packages.ps1
@@ -1,164 +1,135 @@
-function InstrumentExists {
-    param(
-        [string] $ServerParkName,
-        [string] $InstrumentName
+$cmaInstrumentPath = $env:CmaInstrumentPath
+$cmaMultiPackage = $env:CmaMultiPackage
+$cmaServerParkName = $env:CmaServerParkName
+
+$BlaiseConnectionPort = $env:ENV_BLAISE_CONNECTION_PORT
+$BlaiseAdminUser = $env:ENV_BLAISE_ADMIN_USER
+$BlaiseAdminPassword = $env:ENV_BLAISE_ADMIN_PASSWORD
+
+function Test-InstrumentExists {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ServerParkName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$InstrumentName
     )
 
-    If ([string]::IsNullOrEmpty($ServerParkName)) {
-        throw [System.IO.ArgumentException] "No server park name argument provided"
-    }
+    $output = & "C:\blaise5\bin\servermanager.exe" -listsurveys `
+        -serverpark:$ServerParkName `
+        -binding:http `
+        -port:$BlaiseConnectionPort `
+        -user:$BlaiseAdminUser `
+        -password:$BlaiseAdminPassword | Select-String -Pattern $InstrumentName -SimpleMatch
 
-    If ([string]::IsNullOrEmpty($InstrumentName)) {
-        throw [System.IO.ArgumentException] "No instrument name argument provided"
-    }    
-
-    $exists = c:\blaise5\bin\servermanager -listsurveys `
-                                         -serverpark:$ServerParkName `
-                                         -binding:http `
-                                         -port:$env:ENV_BLAISE_CONNECTION_PORT `
-                                         -user:$env:ENV_BLAISE_ADMIN_USER `
-                                         -password:$env:ENV_BLAISE_ADMIN_PASSWORD `
-                                        | findstr -i $InstrumentName                                        
-                       
-    If ([string]::IsNullOrEmpty($exists)) {
-        return $false
-    }
-
-    return $true
+    return ($null -ne $output)
 }
 
-function CheckFileExists {
-    param(
-        [string] $filePath
+function Test-FileExists {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$FilePath
     )
 
-    If ([string]::IsNullOrEmpty($filePath)) {
-        throw [System.IO.ArgumentException] "No file path provided"
-    }
-
-    if (Test-Path $filePath)
-        {
-            return
-        }
-
-    Write-Host "File $filePath does not exist"
-    exit 1        
+    return (Test-Path -Path $FilePath -PathType Leaf)
 }
 
-function UnzipPackage {
-    param(
-        [string] $filePath,
-        [string] $destinationPath
-    )  
+function Expand-ZipFile {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$FilePath,
 
-    If ([string]::IsNullOrEmpty($filePath)) {
-        throw [System.IO.ArgumentException] "No file path provided"
-    }
-
-    If ([string]::IsNullOrEmpty($destinationPath)) {
-        throw [System.IO.ArgumentException] "No destination path provided"
-    }    
-
-    CheckFileExists($filePath)
-
-    try {
-        "Exapnding zip file $filePath to $destinationPath"
-        Expand-Archive -LiteralPath $filePath -DestinationPath $destinationPath -Force
-    }
-    catch {
-        Write-Host "There was an error exapnding zip file $filePath to $destinationPath"
-        exit 1  
-    }
-}
-
-function InstallPackageViaServerManager{
-    param(
-        [string] $ServerParkName,
-        [string] $filePath     
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$DestinationPath
     )
 
-    If ([string]::IsNullOrEmpty($ServerParkName)) {
-        throw [System.IO.ArgumentException] "No server park name provided"
+    if (-not (Test-FileExists -FilePath $FilePath)) {
+        throw "File '$FilePath' does not exist."
     }
 
-    If ([string]::IsNullOrEmpty($filePath)) {
-        throw [System.IO.ArgumentException] "No file path provided"
-    }
-
-    CheckFileExists($filePath)
-
-    try {
-        "Iinstalling the package $filePath into the serverpark $ServerParkName via servermanager"
-        c:\blaise5\bin\servermanager -installsurvey:$filePath `
-                                     -serverpark:$ServerParkName `
-                                     -binding:http `
-                                     -port:$env:ENV_BLAISE_CONNECTION_PORT `
-                                     -user:$env:ENV_BLAISE_ADMIN_USER `
-                                     -password:$env:ENV_BLAISE_ADMIN_PASSWORD
-    }
-    catch {
-        Write-Host "There was an error installing the package $filePath into the serverpark $ServerParkName"
-        exit 1  
-    }     
+    Write-Host "Expanding ZIP file '$FilePath' to '$DestinationPath'"
+    Expand-Archive -LiteralPath $FilePath -DestinationPath $DestinationPath -Force
 }
 
-function InstallPackageViaBlaiseCli{
-    param(
-        [string] $ServerParkName,              
-        [string] $filePath
+function Install-PackageViaServerManager {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ServerParkName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$FilePath
     )
 
-    If ([string]::IsNullOrEmpty($ServerParkName)) {
-        throw [System.IO.ArgumentException] "No server park name provided"
+    if (-not (Test-FileExists -FilePath $FilePath)) {
+        throw "File '$FilePath' does not exist."
     }
 
-    If ([string]::IsNullOrEmpty($filePath)) {
-        throw [System.IO.ArgumentException] "No file path provided"
-    }
-
-    CheckFileExists($filePath)
-    
-    try {
-        Write-Host "Installing the package $filePath into the serverpark $ServerParkName via the CLI"
-        $InstrumentName = [System.IO.Path]::GetFileNameWithoutExtension($filePath)
-        C:\BlaiseServices\BlaiseCli\blaise.cli questionnaireinstall -s $ServerParkName -q $InstrumentName -f $filePath
-       
-    }
-    catch {
-        Write-Host "There was an error installing package $InstrumentName at $filePath into serverpark $ServerParkName"
-        exit 1  
-    }      
+    Write-Host "Installing package '$FilePath' into server park '$ServerParkName' via Server Manager"
+    & "C:\blaise5\bin\servermanager.exe" -installsurvey:$FilePath `
+        -serverpark:$ServerParkName `
+        -binding:http `
+        -port:$BlaiseConnectionPort `
+        -user:$BlaiseAdminUser `
+        -password:$BlaiseAdminPassword
 }
 
-try{
-    # Extract cma packages from multipackage file
-    Write-Host "unzip cma multi package"
-    UnzipPackage -filePath "$env:CmaInstrumentPath\$env:CmaMultiPackage" -destinationPath $env:CmaInstrumentPath
+function Install-PackageViaBlaiseCli {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ServerParkName,
 
-    # Install cma package via servermanager (as it does not contain a database)
-    Write-Host "Install cma package via servermanager"
-    InstallPackageViaServerManager -ServerParkName $env:CmaServerParkName -filePath $env:CmaInstrumentPath\CMA.bpkg
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$FilePath
+    )
 
-    # Install other packages via Bliase CLI to configure the datbaases to be cloud based
-    Write-Host "Install other cma packages via cli"
+    if (-not (Test-FileExists -FilePath $FilePath)) {
+        throw "File '$FilePath' does not exist."
+    }
+
+    $InstrumentName = [System.IO.Path]::GetFileNameWithoutExtension($FilePath)
+    Write-Host "Installing package '$InstrumentName' from '$FilePath' into server park '$ServerParkName' via Blaise CLI"
+    & "C:\BlaiseServices\BlaiseCli\blaise.cli.exe" questionnaireinstall -s $ServerParkName -q $InstrumentName -f $FilePath
+}
+
+try {
+    # Extract CMA packages from multi-package file
+    Write-Host "Unzipping CMA multi-package '$CmaMultiPackage'"
+    Expand-ZipFile -FilePath "$CmaInstrumentPath\$CmaMultiPackage" -DestinationPath $CmaInstrumentPath
+
+    # Install CMA package via Server Manager (as it does not contain a database)
+    Install-PackageViaServerManager -ServerParkName $CmaServerParkName -FilePath "$CmaInstrumentPath\CMA.bpkg"
+
+    # Install other packages via Blaise CLI to configure the databases to be cloud-based
     $InstrumentList = 'CMA_Attempts', 'CMA_ContactInfo', 'CMA_Launcher', 'CMA_Logging'
-    $InstrumentList | ForEach-Object {     
-        if(InstrumentExists -ServerParkName:$env:CmaServerParkName -InstrumentName:$_) {
-            Write-Host "Instrument $_ already exists on $env:CmaServerParkName - don't install"
+    foreach ($Instrument in $InstrumentList) {
+        if (Test-InstrumentExists -ServerParkName $CmaServerParkName -InstrumentName $Instrument) {
+            Write-Host "Instrument '$Instrument' already exists on '$CmaServerParkName' - skipping installation"
         }
         else {
-            InstallPackageViaBlaiseCli -ServerParkName $env:CmaServerParkName `
-            -filePath $env:CmaInstrumentPath\$_.bpkg 
-        }           
-    } 
+            Install-PackageViaBlaiseCli -ServerParkName $CmaServerParkName -FilePath "$CmaInstrumentPath\$Instrument.bpkg"
+        }
+    }
 
-    # Cleanup temporary cma packages folder
-    Remove-Item -LiteralPath $env:CmaInstrumentPath -Force -Recurse
+    # Cleanup temporary CMA packages folder
+    Write-Host "Removing temporary CMA packages folder '$CmaInstrumentPath'"
+    Remove-Item -LiteralPath $CmaInstrumentPath -Force -Recurse
 }
-catch{
-    Write-Host "Installing cma packages failed: $($_.ScriptStackTrace)"
+catch {
+    Write-Error "Installing CMA packages failed: $($_.Exception.Message)"
+    Write-Error "$($_.ScriptStackTrace)"
     exit 1
 }
-
-
-
