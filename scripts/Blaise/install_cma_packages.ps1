9a244d62536a13071fc35c171f8908ba989bafa9
riceyo <richmond@ricey.org>
2024-03-21 21:48:09 +0000

    test different file name split method

--- scripts/Blaise/install_cma_packages.ps1
+++ scripts/Blaise/install_cma_packages.ps1
@@ -120,7 +120,7 @@ function InstallPackageViaBlaiseCli{
     
     try {
         Write-Host "Installing the package $filePath into the serverpark $ServerParkName via the CLI"
-        $InstrumentName = Split-Path -LeafBase $filePath
+        $InstrumentName = [System.IO.Path]::GetFileNameWithoutExtension($filePath)
         C:\BlaiseServices\BlaiseCli\blaise.cli questionnaireinstall -s $ServerParkName -q $InstrumentName -f $filePath
        
     }
