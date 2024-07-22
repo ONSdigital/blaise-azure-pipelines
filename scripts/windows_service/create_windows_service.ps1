6481ba5ab3da1360badfcbd69463d1aa58261e91
Richmond Rice <richmond@ricey.org>
2024-07-22 18:37:12 +0100

    consistant recovery opts

--- scripts/windows_service/create_windows_service.ps1
+++ scripts/windows_service/create_windows_service.ps1
@@ -6,13 +6,10 @@ LogInfo("Creating Windows service $ServiceName")
 sc.exe create $ServiceName binpath=C:\BlaiseServices\$ServiceName\$exeName.exe
 
 LogInfo("Adding recovery options to Windows service $ServiceName")
-sc.exe failure $ServiceName reset= 1 actions= restart/60000/run/1000
+sc.exe failure $ServiceName reset=0 actions=restart/1000
 
 LogInfo("Adding delayed start to Windows service $ServiceName")
-sc.exe config $ServiceName start= delayed-auto
+sc.exe config $ServiceName start=delayed-auto
 
 LogInfo("Starting Windows service $ServiceName")
 sc.exe start $ServiceName
-
-LogInfo("Adding secondary recovery options to Windows service $ServiceName")
-sc.exe failure $ServiceName reset= 7200 actions= restart/0/restart/30000//
