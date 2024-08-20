ccfedd5170cb0770488f43bd9096e46dc751598f
cal-nic <callum.nicholson@ons.gov.uk>
2024-08-20 14:51:38 +0100

    resetting this back to how it was

--- scripts/update_script_environment_variables.ps1
+++ scripts/update_script_environment_variables.ps1
@@ -1,22 +1,8 @@
 . "$PSScriptRoot\logging_functions.ps1"
 
 function GetMetadataVariables {
-    # $variablesFromMetadata = Invoke-RestMethod http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=true -Headers @{ "Metadata-Flavor" = "Google" }
-    # return $variablesFromMetadata | Get-Member -MemberType NoteProperty
-
-    $variableGroupId = $env:PARAMETERS.VarGroup.Id
-    LogInfo("Variable group ID: $variableGroupId")
-    # $variableGroup = Get-AzDevOpsVariableGroupVsts -Id $variableGroupId 
-    # $variableGroupName = $variableGroup.name
-
-    # LogInfo("Variable group name: $variableGroupName")
-    # Get variable names and values from the environment variables
-    $variables = Get-ChildItem Env: | Where-Object { $_.Name -like "$variableGroupName.*" } | ForEach-Object {
-        $variableName = $_.Name.Substring($variableGroupName.Length + 1) # Remove the variable group prefix
-        @{ $variableName = $_.Value }
-    }
-
-    return $variables.Keys
+    $variablesFromMetadata = Invoke-RestMethod http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=true -Headers @{ "Metadata-Flavor" = "Google" }
+    return $variablesFromMetadata | Get-Member -MemberType NoteProperty   
 }
 
 function CreateVariables($variableList) {
