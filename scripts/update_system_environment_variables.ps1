29814a5da5f031de81568ad57933b159b732c79a
lambeb <141648830+lambeb@users.noreply.github.com>
2024-09-12 15:56:57 +0100

    Added code to detect if a variable is a secret. If it is then gcloud secrets is called to retrieve the value.

--- scripts/update_system_environment_variables.ps1
+++ scripts/update_system_environment_variables.ps1
@@ -12,7 +12,16 @@ function CreateVariables($variableList) {
         $pattern = "^(.*?)$([regex]::Escape($varName))(.?=)(.*)"
         $varValue = ($varDefinition -replace $pattern, '$3')
 
-        if ($variable.Name -Like "ENV_*") {
+        if ($variable.Name -Like "ENV_*" -and $varValue -Like "projects/*/secrets/*") {
+
+            $parts = $varValue -split "/"
+            $secret = $parts[3]
+
+            $secretValue = & gcloud secrets versions access latest --secret=$secret
+
+            [System.Environment]::SetEnvironmentVariable($varName, ($secretValue), [System.EnvironmentVariableTarget]::Machine)
+        }
+        elseif ($variable.Name -Like "ENV_*") {
             [System.Environment]::SetEnvironmentVariable($varName, ($varValue), [System.EnvironmentVariableTarget]::Machine)
             LogInfo("System env var - $varName = $varValue")
         }
