0eb6227e712e076102b6e504b1eee2a8e65bdded
Jake West <jacob.west@ons.gov.uk>
2024-09-17 13:25:18 +0100

    Corrected conditional

--- scripts/blaise/check_cma_packages.ps1
+++ scripts/blaise/check_cma_packages.ps1
@@ -4,7 +4,7 @@ $CurrentPath = Get-Location
 
 $Blaise = Get-ItemProperty -Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -match 'blaise' } | Select-Object CMAInstalledPackage
 
-if (!$Blaise) {
+if (!$Blaise.CMAInstalledPackage) {
     LogInfo("CMA is not installed")
     . "$CurrentPath\scripts\blaise\install_cma_packages.ps1"
     Set-ItemProperty -Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" -Name "CMAInstalledPackage" -Value $env:ENV_CMA_MULTI_PACKAGE | Where-Object { $_.DisplayName -match 'blaise' }
