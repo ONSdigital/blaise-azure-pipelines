42c4650dc4ada8f19c1217358139cd586b4e761c
Jake West <jacob.west@ons.gov.uk>
2024-09-17 12:44:45 +0100

    Added check for CMA package versioning

---
+++ scripts/blaise/check_cma_packages.ps1
@@ -0,0 +1,23 @@
+. "$PSScriptRoot\..\logging_functions.ps1"
+
+$CurrentPath = Get-Location
+
+$Blaise = Get-ItemProperty -Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -match 'blaise' } | Select-Object CMAInstalledPackage
+
+if (!$Blaise) {
+    LogInfo("CMA is not installed")
+    . "$CurrentPath\scripts\blaise\install_cma_packages.ps1"
+    Set-ItemProperty -Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" -Name "CMAInstalledPackage" -Value $env:ENV_CMA_MULTI_PACKAGE | Where-Object { $_.DisplayName -match 'blaise' }
+}
+else {
+    
+    $CMAInstalledPackage = $Blaise.CMAInstalledPackage
+    LogInfo("CMA $CMAInstalledPackage installed")
+    if ($Blaise.CMAInstalledPackage -eq $env:ENV_CMA_MULTI_PACKAGE) {
+        "CMA mutli package version already installed"
+    }
+    else {
+        LogInfo("CMA mutli package version needs to be changed")
+        . "$CurrentPath\scripts\blaise\install_cma_packages.ps1"
+    }
+}
