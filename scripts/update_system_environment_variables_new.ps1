0fc0aac82eca6cdf78e1351a3f06a8f4f303c87a
lambeb <141648830+lambeb@users.noreply.github.com>
2024-09-20 11:27:15 +0100

    Added Update Environmental Changes

--- scripts/update_system_environment_variables_new.ps1
+++ scripts/update_system_environment_variables_new.ps1
@@ -5,6 +5,33 @@ function GetMetadataVariables {
     return $variablesFromMetadata | Get-Member -MemberType NoteProperty
 }
 
+function UpdateEnvironmentalVariable($varName, $secretValue, $secret) {
+
+    Write-Host "UpdateEnvironmentalVariables Called with following values"
+    Write-Host "varName = $varName"
+    Write-Host "secretValue = $secretValue"
+    Write-Host "secret = $secret"
+
+    $envValue = [System.Environment]::GetEnvironmentVariable($varName, [System.EnvironmentVariableTarget]::Machine)
+
+    Write-Host "Retrieved the following value from Environmental Variables"
+    Write-Host "envValue = $envValue"
+
+    if ($envValue -eq $secretValue) {
+        Write-Host  "Values are the same, doing nothing"
+    }
+    elseif ($envValue -eq "" -or $envValue -eq $null) {
+        Write-Host "Environmental Variable not set, so using Secret value"
+        [System.Environment]::SetEnvironmentVariable($varName, ($secretValue), [System.EnvironmentVariableTarget]::Machine)
+    }
+    elseif ($envValue -ne "" -and $envValue -ne $null -and $secretValue -ne "" -and $null -ne $secretValue) {
+        # This is for environments that have been previously set up, so the secret values should remain the same
+        Write-Host "Environmental Variable is set to a different value than Secret, Creating new secret version"
+        # echo -n $envValue | gcloud secrets versions add $secret --data-file=-     
+        Write-Output -NoNewline $envValue | gcloud secrets versions add $secret --data-file=-
+    }
+}
+
 function CreateVariables($variableList) {
     foreach ($variable in $variableList) {
         $varName = $variable.Name
@@ -19,8 +46,7 @@ function CreateVariables($variableList) {
 
             $secretValue = & gcloud secrets versions access latest --secret=$secret
 
-            [System.Environment]::SetEnvironmentVariable($varName, ($secretValue), [System.EnvironmentVariableTarget]::Machine)
-            LogInfo("BENNY1 Secret Update System Environment Variables - $varName = $secretValue")
+            UpdateEnvironmentalVariable($variable.Name, $secretValue, $secret)
         }
         elseif ($variable.Name -Like "ENV_*") {
             [System.Environment]::SetEnvironmentVariable($varName, ($varValue), [System.EnvironmentVariableTarget]::Machine)
