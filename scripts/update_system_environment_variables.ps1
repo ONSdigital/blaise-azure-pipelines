44e9793955ada25c19897d38a013313601dc5a1d
lambeb <141648830+lambeb@users.noreply.github.com>
2024-10-01 15:16:49 +0100

    still BOM getting added, trying old way with NoNewLine and utf8

--- scripts/update_system_environment_variables.ps1
+++ scripts/update_system_environment_variables.ps1
@@ -35,8 +35,7 @@ function UpdateEnvironmentalVariable {
         Write-Host "Environmental Variable is set to a different value than Secret, Creating new secret version"
         
         $tempFile = New-TemporaryFile
-        #Set-Content -Path $tempFile -Value $envValue  
-        $envValue | Out-File -FilePath $tempFile -NoNewline -Encoding utf8
+        Set-Content -Path $tempFile -Value $envValue  -NoNewline -Encoding utf8  
         & gcloud secrets versions add $secret --data-file=$tempFile
         Remove-Item $tempFile
     } 
