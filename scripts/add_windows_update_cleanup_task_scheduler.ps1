f376375a4fcb7df4b4cc6e2730f4b14788035d17
lambeb <141648830+lambeb@users.noreply.github.com>
2025-01-02 16:00:19 +0000

    Initial commit of script and changes to configure_blaise.yml

---
+++ scripts/add_windows_update_cleanup_task_scheduler.ps1
@@ -0,0 +1,19 @@
+function Set-UpdateCleanupStateFlags {
+    Write-Host 'Enabling Update Cleanup.'
+    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Update Cleanup' -Name StateFlags0099 -Value 2 -Type DWord
+}
+    
+$taskName = "WindowsUpdateCleanup"  # Set the task name
+$executionTime = "22:00"            # Set the desired execution time (24-hour format)
+$executionDay = "1"                 # Set the desired execution day (1-31)
+    
+# Check if a task with the same name already exists
+if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
+    Write-Host "Task '$taskName' already exists. Skipping creation."
+} 
+else {
+    # Call function to set Windows Update Flags for Cleanup
+    Set-UpdateCleanupStateFlags
+    schtasks.exe /Create /SC MONTHLY /D $executionDay /TN $taskName /TR "cleanmgr.exe /sagerun:99" /ST $executionTime /RL HIGHEST
+    Write-Host "Task '$taskName' created successfully."
+}
