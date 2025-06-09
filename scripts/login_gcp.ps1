638f3a61a53210566d815f214e216c073efe71b7
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-06-09 11:29:10 +0100

    revert

--- scripts/login_gcp.ps1
+++ scripts/login_gcp.ps1
@@ -1,5 +1,4 @@
 . "$PSScriptRoot\logging_functions.ps1"
 
-$env:PATH += ";C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin"
 LogInfo("Logging into GCP with VM service account")
 gcloud auth login $env:ENV_VM_SERVICEACCOUNT
