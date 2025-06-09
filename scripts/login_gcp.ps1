7a7a61261a9468184ae02c602eed8ea7f68c933c
kristian4res <57638182+kristian4res@users.noreply.github.com>
2025-06-09 11:22:09 +0100

    wip: add gcloud cli install step and add to env path

--- scripts/login_gcp.ps1
+++ scripts/login_gcp.ps1
@@ -1,4 +1,5 @@
 . "$PSScriptRoot\logging_functions.ps1"
 
+$env:PATH += ";C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin"
 LogInfo("Logging into GCP with VM service account")
 gcloud auth login $env:ENV_VM_SERVICEACCOUNT
