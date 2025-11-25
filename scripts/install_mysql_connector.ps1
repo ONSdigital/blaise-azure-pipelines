56bb1c32b0f0d6bd4f4fc2d825bc4516fe7e9fe6
Richmond Rice <richmond@ricey.org>
2025-11-25 14:58:14 +0000

    update mysql connector

--- scripts/install_mysql_connector.ps1
+++ scripts/install_mysql_connector.ps1
@@ -2,11 +2,11 @@ param ([string]$GCP_BUCKET)
 
 . "$PSScriptRoot\logging_functions.ps1"
 
-if (Test-Path C:\dev\data\mysql-connector-net-8.0.22.msi) {
+if (Test-Path C:\dev\data\mysql-connector-net-9.5.0.msi) {
     LogInfo("MySQL connector already installed, skipping...")
 }
 else {
     LogInfo("Downloading and installing MySQL connector...")
-    gsutil cp gs://$GCP_BUCKET/mysql-connector-net-8.0.22.msi "C:\dev\data\mysql-connector-net-8.0.22.msi"
-    Start-Process msiexec.exe -Wait -ArgumentList '/I C:\dev\data\mysql-connector-net-8.0.22.msi /quiet'
+    gsutil cp gs://$GCP_BUCKET/mysql-connector-net-9.5.0.msi "C:\dev\data\mysql-connector-net-9.5.0.msi"
+    Start-Process msiexec.exe -Wait -ArgumentList '/I C:\dev\data\mysql-connector-net-9.5.0.msi /quiet'
 }
