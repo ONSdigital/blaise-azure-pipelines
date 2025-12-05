03505927edae8f440d3f91019045d504d4b1f834
SidraJaved <16195224+SidraJaved@users.noreply.github.com>
2025-12-05 10:45:36 +0000

    wip-3

--- scripts/download_files_from_shared_bucket.ps1
+++ scripts/download_files_from_shared_bucket.ps1
@@ -43,20 +43,20 @@ function Get-AzureOidcToken {
     return $response.oidcToken
 }
 
-function CheckDefaultServiceAccountActivation {
+# function CheckDefaultServiceAccountActivation {
 
-    # Write-Host "📡 Validating access token (should come from metadata)..."
-    # $active = gcloud auth list --filter="status:ACTIVE" --format="value(account)" 2>$null
-    # Write-Host "Active account: $active"
+#     # Write-Host "📡 Validating access token (should come from metadata)..."
+#     # $active = gcloud auth list --filter="status:ACTIVE" --format="value(account)" 2>$null
+#     # Write-Host "Active account: $active"
 
-    $token = gcloud auth print-access-token 2>$null
+#     $token = gcloud auth print-access-token 2>$null
 
-    if ($LASTEXITCODE -eq 0 -and $token.Length -gt 100) {
-        Write-Host "✅ VM now using metadata service account"
-    } else {
-        Write-Host "❌ Token retrieval failed — metadata SA not active"
-    }
-}
+#     if ($LASTEXITCODE -eq 0 -and $token.Length -gt 100) {
+#         Write-Host "✅ VM now using metadata service account"
+#     } else {
+#         Write-Host "❌ Token retrieval failed — metadata SA not active"
+#     }
+# }
 
 try {
     Write-Host "⚙️ Starting GCP authentication with WIF using SA impersonation..."
@@ -154,6 +154,6 @@ finally {
     Write-Host "🔄 Activating default configuration..."
     gcloud config configurations activate default --quiet
 
-    CheckDefaultServiceAccountActivation
+    # CheckDefaultServiceAccountActivation
     Write-Host "✨ Cleanup complete."
 }
