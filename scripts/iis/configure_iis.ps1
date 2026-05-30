998d3f17049740a0f30d64db0a70b108239e639f
Richmond Rice <richmond@ricey.org>
2026-05-30 22:09:14 +0100

    516

--- scripts/iis/configure_iis.ps1
+++ scripts/iis/configure_iis.ps1
@@ -14,6 +14,10 @@ Start-Process msiexec.exe -Wait -ArgumentList '/I C:\dev\data\rewrite_url.msi /q
 
 DisableCompression
 
+$externalServerName = "https://$env:ENV_BLAISE_CATI_URL"
+$encodedExternalServerName = [System.Uri]::EscapeDataString($externalServerName)
+$escapedExternalServerName = $externalServerName.Replace("/", "\\/")
+
 $sites = @("Blaise", "BlaiseDashboard")
 $existingSites = $sites | Where-Object { Test-Path "iis:\sites\Default Web Site\$_" }
 
@@ -24,8 +28,23 @@ if (-not $existingSites) {
 
 foreach ($site in $existingSites) {
     AddNoCompressionPreCondition -siteName $site
-    AddRewriteRule -siteName $site -ruleName "Blaise data entry" -serverName "https://$env:ENV_BLAISE_CATI_URL" -rule "http://blaise-gusty-data[^/]*"
-    AddRewriteRule -siteName $site -ruleName "Blaise mgmt" -serverName "https://$env:ENV_BLAISE_CATI_URL" -rule "http://blaise-gusty-mgmt*"
+
+    # Rewrite plain internal Blaise hosts emitted in HTML/text responses.
+    AddRewriteRule -siteName $site -ruleName "Blaise data entry" -serverName "$externalServerName{R:1}" -rule 'https?://blaise-[^/\s"<>]*-data[^/\s"<>]*(/[^\s"<>]*)?'
+    AddRewriteRule -siteName $site -ruleName "Blaise mgmt" -serverName "$externalServerName{R:1}" -rule 'https?://blaise-[^/\s"<>]*-mgmt[^/\s"<>]*(/[^\s"<>]*)?'
+
+    # Rewrite URL-encoded internal hosts used by newer dashboard StartSurvey links.
+    AddRewriteRule -siteName $site -ruleName "Blaise data entry encoded" -serverName "$encodedExternalServerName{R:1}" -rule 'https?%3a%2f%2fblaise-[^%\s"&<>]*-data[^%\s"&<>]*(%2f[^\s"&<>]*)?'
+    AddRewriteRule -siteName $site -ruleName "Blaise mgmt encoded" -serverName "$encodedExternalServerName{R:1}" -rule 'https?%3a%2f%2fblaise-[^%\s"&<>]*-mgmt[^%\s"&<>]*(%2f[^\s"&<>]*)?'
+
+    # Rewrite JSON-escaped internal hosts (for responses that emit http:\/\/host style strings).
+    AddRewriteRule -siteName $site -ruleName "Blaise data entry escaped" -serverName "$escapedExternalServerName{R:1}" -rule 'https?:\\/\\/blaise-[^\\/\s"<>]*-data[^\\/\s"<>]*(\\/[^\s"<>]*)?'
+    AddRewriteRule -siteName $site -ruleName "Blaise mgmt escaped" -serverName "$escapedExternalServerName{R:1}" -rule 'https?:\\/\\/blaise-[^\\/\s"<>]*-mgmt[^\\/\s"<>]*(\\/[^\s"<>]*)?'
+
+    # Rewrite redirect Location headers that still point to internal hosts.
+    AddRewriteRule -siteName $site -ruleName "Blaise data entry location header" -serverName "$externalServerName{R:1}" -rule '^https?://blaise-[^/\s"<>]*-data[^/\s"<>]*(/.*)?$' -serverVariable "RESPONSE_LOCATION" -preCondition ""
+    AddRewriteRule -siteName $site -ruleName "Blaise mgmt location header" -serverName "$externalServerName{R:1}" -rule '^https?://blaise-[^/\s"<>]*-mgmt[^/\s"<>]*(/.*)?$' -serverVariable "RESPONSE_LOCATION" -preCondition ""
+
     RemoveWebDav -siteName $site
 
     $appPool = "$($site)AppPool"
