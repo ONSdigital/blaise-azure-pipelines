3e1d3828e65d53f48ab705faa4151010bc0a8085
Richmond Rice <richmond@ricey.org>
2026-05-30 18:38:17 +0100

    516

--- scripts/iis/configure_iis.ps1
+++ scripts/iis/configure_iis.ps1
@@ -26,10 +26,10 @@ if (-not $existingSites) {
 
 foreach ($site in $existingSites) {
     AddNoCompressionPreCondition -siteName $site
-    AddRewriteRule -siteName $site -ruleName "Blaise data entry" -serverName "https://$env:ENV_BLAISE_CATI_URL" -rule "https?://blaise-gusty-data[^/]*" -preCondition ""
-    AddRewriteRule -siteName $site -ruleName "Blaise mgmt" -serverName "https://$env:ENV_BLAISE_CATI_URL" -rule "https?://blaise-gusty-mgmt[^/]*" -preCondition ""
-    AddRewriteRule -siteName $site -ruleName "Blaise data entry encoded" -serverName $encodedServerName -rule "https?%3a%2f%2fblaise-gusty-data[^%]*" -preCondition ""
-    AddRewriteRule -siteName $site -ruleName "Blaise mgmt encoded" -serverName $encodedServerName -rule "https?%3a%2f%2fblaise-gusty-mgmt[^%]*" -preCondition ""
+    AddRewriteRule -siteName $site -ruleName "Blaise data entry" -serverName "https://$env:ENV_BLAISE_CATI_URL" -rule "https?://blaise-gusty-data[^/]*"
+    AddRewriteRule -siteName $site -ruleName "Blaise mgmt" -serverName "https://$env:ENV_BLAISE_CATI_URL" -rule "https?://blaise-gusty-mgmt[^/]*"
+    AddRewriteRule -siteName $site -ruleName "Blaise data entry encoded" -serverName $encodedServerName -rule "https?%3a%2f%2fblaise-gusty-data[^%]*"
+    AddRewriteRule -siteName $site -ruleName "Blaise mgmt encoded" -serverName $encodedServerName -rule "https?%3a%2f%2fblaise-gusty-mgmt[^%]*"
     AddRewriteRule -siteName $site -ruleName "Blaise data entry location header" -serverName "https://$env:ENV_BLAISE_CATI_URL{R:2}" -rule "^https?://blaise-gusty-data(:\d+)?(.*)$" -serverVariable "RESPONSE_LOCATION" -preCondition ""
     AddRewriteRule -siteName $site -ruleName "Blaise mgmt location header" -serverName "https://$env:ENV_BLAISE_CATI_URL{R:2}" -rule "^https?://blaise-gusty-mgmt(:\d+)?(.*)$" -serverVariable "RESPONSE_LOCATION" -preCondition ""
     AddRewriteRule -siteName $site -ruleName "Blaise data entry encoded location header" -serverName $encodedServerName -rule "https?%3a%2f%2fblaise-gusty-data[^%]*" -serverVariable "RESPONSE_LOCATION" -preCondition ""
